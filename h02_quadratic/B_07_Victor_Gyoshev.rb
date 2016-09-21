49af40b53d1913a77558baff5f98cd4393a1b3f2
Catishere <viktor.gyoshev@gmail.com>
2016-09-22 02:19:30 +0300

    Create B_07_Victor_Gyoshev.rb

---
+++ h02_quadratic/B_07_Victor_Gyoshev.rb
@@ -0,0 +1,9 @@
+argument = gets.split(" ").map! { |a| a.to_i }
+result = Array.new
+d = argument[1]**2-4*argument[0]*argument[2]
+if d>0 then
+	result[0] = (-argument[1] + Math::sqrt(d))/(2*argument[0])
+	result[1] = (-argument[1] - Math::sqrt(d))/(2*argument[0])
+	result.map! { |x| if x.to_i == x then x.to_i else x.round(2) end}
+end
+puts result.sort.join(",")
