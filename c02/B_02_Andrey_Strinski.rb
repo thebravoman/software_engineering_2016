68f931ad7e1e53c48fd4a85b25479a72da9cbdff
Andrey Strinski <Kroxitrock@gmail.com>
2016-09-21 12:38:41 +0300

    Add files via upload

---
+++ c02/B_02_Andrey_Strinski.rb
@@ -0,0 +1,3 @@
+puts Dir.glob(File.join("/home/elsyser/Desktop/zad1/software_engineering_2016-master/c01_introduction", '**', '*.txt')).select { |file| File.file?(file) }.count
+
+puts Dir("/home/elsyser/Desktop/zad1/software_engineering_2016-master/c01_introduction/*.txt",')
