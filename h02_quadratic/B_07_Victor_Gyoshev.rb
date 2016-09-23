3fe595efb715e1480bafd0f58514858d2cbc2947
Catishere <viktor.gyoshev@gmail.com>
2016-09-23 05:23:42 +0300

    wtf im doin

--- h02_quadratic/B_07_Victor_Gyoshev.rb
+++ h02_quadratic/B_07_Victor_Gyoshev.rb
@@ -1,9 +1,16 @@
-argument = gets.split(" ").map! { |a| a.to_i }
+a = ARGV[0].to_i; b = ARGV[1].to_i; c = ARGV[2].to_i
 result = Array.new
-d = argument[1]**2-4*argument[0]*argument[2]
+if a == 0
+	if -c/b == -c/b.to_f
+		print -c/b 
+		else print (-c/b.to_f).round(2)
+	end
+exit(1) 
+end
+d = b**2-4*a*c
 if d>=0 then
-	result[0] = (-argument[1] + Math::sqrt(d))/(2*argument[0])
-	result[1] = (-argument[1] - Math::sqrt(d))/(2*argument[0])
+	result[0] = (-b + Math::sqrt(d))/(2*a)
+	result[1] = (-b - Math::sqrt(d))/(2*a)
 	result.map! { |x| if x.to_i == x then x.to_i else x.round(2) end}
 end
-if d=0 then print result[0] else print result.sort.join(",") end
+if d == 0 then print result[0] else print result.sort.join(",") end
