ba26cf4669d115f7386c5a221114d9243760d332
IvoTod <ivo.tod.todorov@gmail.com>
2016-09-23 17:22:31 +0300

    Upload homework 02

---
+++ h02_quadratic/A_14_Ivo_Todorov.rb
@@ -0,0 +1,51 @@
+include Math
+
+a = ARGV[0].to_f
+b = ARGV[1].to_f
+c = ARGV[2].to_f
+if a == 0 
+	x = -c/b
+	if b < 0
+		x = -x
+	end
+	if x % 1 == 0
+		x = x.to_i
+		print "#{x}"
+	else
+		print "#{x.round(2)}"
+	end
+else
+	d = b**2 - (4 * a * c)
+
+	if d>0
+		x1 = (-b + sqrt(d))/(2*a)
+		x2 = (-b - sqrt(d))/(2*a)
+		if x1>x2
+			temp = x1
+			x1 = x2
+			x2 = temp
+		end
+		if x1 % 1 == 0
+			x1 = x1.to_i
+			print "#{x1},"
+		else
+			print "#{x1.round(2)},"
+		end
+		
+		if x2 % 1 == 0
+			x2 = x2.to_i
+			print "#{x2}"
+		else
+			print "#{x2.round(2)}"
+		end
+		
+	elsif d==0
+		x = (-b + Math.sqrt(d))/(2*a)
+		if x % 1 == 0
+			x = x.to_i
+			print "#{x}"
+		else
+			print "#{x.round(2)}"
+		end
+	end
+end
