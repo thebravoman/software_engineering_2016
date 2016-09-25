d822895e52a8b87eb6be2e690e4c22b6c69f070e
Slavkata <slavkirilov00@gmail.com>
2016-09-25 22:59:39 +0300

    Update B_07_Victor_Gyoshev.rb

--- h02_quadratic/B_07_Victor_Gyoshev.rb
+++ h02_quadratic/B_07_Victor_Gyoshev.rb
@@ -1,4 +1,6 @@
-a = ARGV[0].to_i; b = ARGV[1].to_i; c = ARGV[2].to_i
+a = ARGV[0].to_i
+b = ARGV[1].to_i
+c = ARGV[2].to_i
 result = Array.new
 if a == 0
 	if b != 0
@@ -10,10 +12,13 @@ if a == 0
 	end 
 exit(1) 
 end
-d = b**2-4*a*c
-if d>=0 then
+d = b**2 - 4*a*c
+if d >= 0 then
 	result[0] = (-b + Math::sqrt(d))/(2*a)
 	result[1] = (-b - Math::sqrt(d))/(2*a)
 	result.map! { |x| if x.to_i == x then x.to_i else x.round(2) end}
 end
-if d == 0 then puts result[0] else puts result.sort.join(",") end
+if d == 0 then
+	puts result[0] 
+	else puts result.sort.join(",")
+end
