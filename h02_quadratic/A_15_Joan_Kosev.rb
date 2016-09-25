44126374cf1a0dff83df99998ca40db02a33444f
Joan Kosev <Joankosev@gmail.com>
2016-09-25 17:58:40 +0300

    Update A_15_Joan_Kosev.rb

--- h02_quadratic/A_15_Joan_Kosev.rb
+++ h02_quadratic/A_15_Joan_Kosev.rb
@@ -4,13 +4,17 @@ a=ARGV[0].to_f
 b=ARGV[1].to_f
 c=ARGV[2].to_f
 
-if a == 0
+if (a == 0 && b == 0 && c == 00)
+	print "#\n"
+
+
+elsif a == 0
 	x_0 = -c/b
 	if x_0 % 1 == 0
 		x_0.to_i = x_0.to_i
-		print "#{x_0}"
+		print "#{x_0}\n"
 	else
-		print "#{x_0.round(2)}"
+		print "#{x_0.round(2)}\n"
 	end
 else
 	d = b*b - (4 * a * c)
@@ -20,9 +24,9 @@ else
 
 		if x_0 % 1 == 0
 			x_0 = x_0.to_i
-			print "#{x_0}"
+			print "#{x_0}\n"
 		else
-			print "#{x_0.round(2)}"
+			print "#{x_0.round(2)}\n"
 		end
 	end
 
@@ -41,9 +45,9 @@ else
 		
 		if x_2 % 1 == 0
 			x_2 = x_2.to_i
-			print "#{x_2}"
+			print "#{x_2}\n"
 		else
-			print "#{x_2.round(2)}"
+			print "#{x_2.round(2)}\n"
 		end
 		
 		else
@@ -55,9 +59,9 @@ else
 		end
 		if x_1 % 1 == 0
 			x_1 = x_1.to_i
-			print "#{x_1}"
+			print "#{x_1}\n"
 		else
-			print "#{x_1.round(2)}"
+			print "#{x_1.round(2)}\n"
 		end
 		end
 		
