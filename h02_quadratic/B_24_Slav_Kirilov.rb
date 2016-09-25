db22cc1a02d3b960c71411588ee0d0857bd8f5cf
Slavkata <slavkirilov00@gmail.com>
2016-09-25 22:54:39 +0300

    Update B_24_Slav_Kirilov.rb

--- h02_quadratic/B_24_Slav_Kirilov.rb
+++ h02_quadratic/B_24_Slav_Kirilov.rb
@@ -1,7 +1,7 @@
-a = gets.to_f
-b = gets.to_f
-c = gets.to_f
-if a == 0 then
+a = ARGV[0].to_f
+b = ARGV[1].to_f
+c = ARGV[2].to_f
+if a == 0 && b != 0 && c != 0 then
    x1 = c/b.round(2)
 	printf("%g\n", x1)
  elsif a != 0
@@ -13,12 +13,12 @@ if a == 0 then
       		 	x1 = ((-b-Math.sqrt(D))/2/a).round(2)
      		 	x2 = ((-b+Math.sqrt(D))/2/a).round(2)
 		 	if(x1 < x2) 
-				printf("%g,%g\n", x1, x2)
+				printf("%g,%g\n",x1 ,x2)
 		 	 else 
-			 	 printf("%g,%g\n", x2, x1)
+			 	 printf("%g,%g\n" ,x2 ,x1)
 			 end
   		end  
  end
-if b == 0 && a == 0 && c == 0 then 
+if b == 0 && a == 0 && c == 0 
 	puts "#"
 end
