664c116cf360ce8834e5f8fc39ea712dcb74303a
ivanmilevtues <ivanmilevtues@gmail.com>
2016-09-25 10:56:03 -0700

    2nd Homework

---
+++ h02_quadratic/A_11_Ivan_Milev.rb
@@ -0,0 +1,37 @@
+a = ARGV[0].to_f
+b = ARGV[1].to_f
+c = ARGV[2].to_f
+
+d = (b * b) - (4 * a * c)
+if a == 0 && (b != 0 || c != 0)
+  x1 = (-c / b).to_f
+  if(x1 == x1.to_i)
+    x1 = x1.to_i
+  end
+  puts "#{(x1.is_a? Float) ? x1.round(2) : x1 }"
+elsif a == 0 && b == 0 && c == 0
+  puts "#"
+else
+  if d > 0
+    #puts "Jello"
+    x1 = ((-b) + (Math.sqrt(d))) / (2 * a)
+    x2 = ((-b) - (Math.sqrt(d))) / (2 * a)
+    #puts "??#{x1},#{x2}??"
+    if(x1 == x1.to_i)
+      x1 = x1.to_i
+    end
+    if(x2 == x2.to_i)
+      x2 = x2.to_i
+    end
+    puts "#{(x1.is_a? Float) ? x1.round(2) : x1 },#{(x2.is_a? Float) ? x2.round(2) : x2}" if x1 < x2
+    puts "#{(x2.is_a? Float) ? x2.round(2) : x2},#{(x1.is_a? Float) ? x1.round(2) : x1 }" if x1 > x2
+
+
+  elsif d == 0
+    x1 = -b / 2 * a
+    if(x1 == x1.to_i)
+      x1 = x1.to_i
+    end
+    puts "#{(x1.is_a? Float) ? x1.round(2) : x1}"
+  end
+end
