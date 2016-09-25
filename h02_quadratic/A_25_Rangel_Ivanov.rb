9143720c13194eabc5b214c15c437a162d8c0449
radito3 <radito3@mail.bg>
2016-09-25 17:57:46 +0300

    Update A_25_Rangel_Ivanov.rb

--- h02_quadratic/A_25_Rangel_Ivanov.rb
+++ h02_quadratic/A_25_Rangel_Ivanov.rb
@@ -13,17 +13,32 @@ if a == 0
         exit
     else
     x = -c/b
-    puts x.round(2)
+        if x % 1 == 0 
+            x = x.to_i
+        else 
+            x = x.round(2)
+        end
+    puts x.to_s
     exit
 end
 else if b == 0
     x1 = Math.sqrt(-c/a)
     x2 = -Math.sqrt(-c/a)
+    if x1 % 1 == 0
+        x1 = x1.to_i
+    else
+        x1 = x1.round(2)
+    end
+    if x2 % 1 == 0
+        x2 = x2.to_i
+    else
+        x2 = x2.round(2)
+    end
     if x1 > x2
-        puts x2.round(2) + "," + x1.round(2)
+        puts x2.to_s + "," + x1.to_s
         exit
     else
-        puts x1.round(2) + "," + x2.round(2)
+        puts x1.to_s + "," + x2.to_s
         exit
     end
 end
@@ -33,15 +48,30 @@ if d.real < 0
     exit
 else if d.real == 0
     x = -b/2*a
-    puts x.round(2)
+    if x % 1 == 0
+        x = x.to_i
+    else
+        x = x.round(2)
+    end
+    puts x.to_s
     exit
 end
 end
 x1 = (-b+d)/2*a
 x2 = (-b-d)/2*a
-if x1.real > x2.real
-    puts x2.real.round(2).to_s + "," + x1.real.round(2).to_s
+if x1 % 1 == 0
+    x1 = x1.to_i
 else
-    puts x1.real.round(2).to_s + "," + x2.real.round(2).to_s
+    x1 = x1.round(2)
+end
+if x2 % 1 == 0 
+    x2 = x2.to_i
+else
+    x2 = x2.round(2)
+end
+if x1.real > x2.real
+    puts x2.real.to_s + "," + x1.real.to_s
+else
+    puts x1.real.to_s + "," + x2.real.to_s
 end
 end
