ced68f6db45f9ef7951088759c36843af1e165a1
Slavkata <slavkirilov00@gmail.com>
2016-09-25 22:30:46 +0300

    Update B_24_Slav_Kirilov.rb

--- h02_quadratic/B_24_Slav_Kirilov.rb
+++ h02_quadratic/B_24_Slav_Kirilov.rb
@@ -1,22 +1,24 @@
-A = gets.to_f
-if (A == 0)
-   puts 'Not a quadratic equation.'
-   return 0
-end
-B = gets.to_f
-C = gets.to_f
-D = B*B - 4*A*C
-if (D == 0)
-    x = (-B/2/A).to_f
-    puts x.to_s	
- else
-   if (D > 0)
-      x1 = ((-B-Math.sqrt(D))/2/A).to_f
-      x2 = ((-B+Math.sqrt(D))/2/A).to_f
-	if(x1 < x2) 
-		puts x1.to_s + ',' + x2.to_s
-	 else 
-		 puts x2.to_s + ',' + x1.to_s
-	 end
-  end  
+a = gets.to_f
+b = gets.to_f
+c = gets.to_f
+if a == 0 then
+   x1 = c/b.round(2)
+	printf("%g\n", x1)
+ elsif a != 0
+ 	D = b*b - 4*a*c
+	 	if (D == 0)
+    		 	x = (-b/2/a).round(2)
+    		 	printf("%g\n", x)	
+ 	 	elsif (D > 0)
+      		 	x1 = ((-b-Math.sqrt(D))/2/a).round(2)
+     		 	x2 = ((-b+Math.sqrt(D))/2/a).round(2)
+		 	if(x1 < x2) 
+				printf("%g,%g\n", x1, x2)
+		 	 else 
+			 	 printf("%g,%g\n", x2, x1)
+			 end
+  		end  
+ end
+if b == 0 && a == 0 && c == 0 then 
+	puts "#"
 end
