9615c935cc15d3ab3bc70e7283adc9fba56437e2
iordanivanov <iiordan.ivanov@gmail.com>
2016-09-25 19:11:54 +0300

    Update A_16_Iordan_Ivanov.rb

--- h02_quadratic/A_16_Iordan_Ivanov.rb
+++ h02_quadratic/A_16_Iordan_Ivanov.rb
@@ -5,36 +5,40 @@ c = ARGV[2].to_f
 D = b**2 - 4*a*c
 
 if a == 0
-	x = c/b
-	if x % 1 == 0
-		print "#{x.to_i}"
+	x = -c/b
+	if b == 0
+		if c == 0
+			puts "#"
+		end
+	elsif x % 1 == 0
+		puts "#{x.to_i}"
 	else
-		print "#{(x * 100).floor / 100.0}"
+		puts "#{(x * 100).floor / 100.0}"
 	end
 else
 	if D < 0
 
 	elsif D == 0
-		x = b / (2 * a)
+		x = -b / (2 * a)
 		if x % 1 == 0
-			print "#{x.to_i}"
+			puts "#{x.to_i}"
 		else
-			print "#{(x * 100).floor / 100.0}"
+			puts "#{(x * 100).floor / 100.0}"
 		end
 	elsif D > 0
 		x1 = (-b + Math.sqrt(D)) / (2 * a)
 		x2 = (-b - Math.sqrt(D)) / (2 * a)
 		if x1 % 1 == 0 && x2 % 1 == 0 
 			if x1 < x2
-				print "#{x1.to_i},#{x2.to_i}"
+				puts "#{x1.to_i},#{x2.to_i}"
 			else
-				print "#{x2.to_i},#{x1.to_i}"
+				puts "#{x2.to_i},#{x1.to_i}"
 			end
 		else
 			if x1 < x2
-				print "#{(x1 * 100).floor / 100.0},#{(x2 * 100).floor / 100.0}"
+				puts "#{(x1 * 100).floor / 100.0},#{(x2 * 100).floor / 100.0}"
 			else
-				print "#{(x2 * 100).floor / 100.0},#{(x1 * 100).floor / 100.0}"
+				puts "#{(x2 * 100).floor / 100.0},#{(x1 * 100).floor / 100.0}"
 			end
 		end
 	end
