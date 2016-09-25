408fd3703f8dd655ef58c6cbbcae00532368c948
Boris Mutafov <mutafow@gmail.com>
2016-09-25 19:51:06 +0300

    Update wtf-.-

--- h02_quadratic/B_03_Boris_Mutafov.rb
+++ h02_quadratic/B_03_Boris_Mutafov.rb
@@ -1,49 +1,57 @@
-#Getting inputs
-param_a = gets.to_f
-param_b = gets.to_f
-param_c = gets.to_f
+a = Float(gets)
+b = Float(gets)
+c = Float(gets)
+check_if_int = 0
 
-
-#Calculations
-if param_a == 0 then
-	if param_b != 0 then	
-		result = (param_c - 2*param_c) / param_b
-	end
-else 
-	discriminant = param_b*param_b - 4*param_a*param_c
-	if discriminant >= 0 then 
-		square = Math.sqrt(discriminant.to_f)
-		x1 = (-param_b + square ) /(2 * param_a)
-		x2 = (-param_b - square ) /(2 * param_a)
-	end
+if a == 0 then
+  if b == 0 then
+    if c == 0 then
+      result = "#"
+    else
+      result =  "No solutions."
+    end
+  else
+    result = Float(-c/b).round(2)
+    check_if_int = 1
+  end
+else
+  d = b*b - 4*a*c
+  if d > 0 then
+    check_if_int = 2
+    result = Array.new
+    x = ( -b + Math.sqrt(d) ) / (2 * a)
+    result[0] = x.round(2)
+    x = ( -b - Math.sqrt(d) ) / (2 * a)
+    result[1] = x.round(2)
+    if result[0] > result[1] then
+      result[0], result[1] = result[1], result[0]
+    end
+  elsif d == 0 then
+    result = ( ( -b ) / (2 * a) ).round(2)
+    check_if_int = 1
+  else
+    result = "No solutions."
+  end
 end
 
-#Printing
+if check_if_int == 1 && result % 1 == 0 then
+  result = Integer(result)
+end
 
-unless param_a != 0 then
-	if param_b != 0 then
-		puts sprintf('%.2f', result)
-	else
-		if param_c != 0 then
-			puts "No solution. Cannot divide by zero."
-		else
-			puts "All numbers are solutions."
-		end
-	end
-else 
-	unless discriminant < 0 then
-		if x1 == x2 then
-			puts sprintf('%.2f', x1)
-		else 
-			if x1 < x2 then
-				puts sprintf('%.2f', x1)
-				puts sprintf('%.2f', x2)
-			else 
-				puts sprintf('%.2f', x2)
-				puts sprintf('%.2f', x1)
-			end
-		end
-	else 
-		puts "No Real Solutions."
-	end 
+if check_if_int == 2 then
+  if result[0] % 1 == 0 then
+    print String(Integer(result[0])) << ','
+  else
+    print String(result[0]) << ','
+  end
+
+  if result[1] % 1 == 0 then
+    print String(Integer(result[1])) << "\n"
+  else
+    print String(result[1]) << "\n"
+  end
+end
+
+if check_if_int != 2 then
+  puts result
 end
