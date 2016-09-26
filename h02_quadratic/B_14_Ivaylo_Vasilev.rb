880853d0243bac906b9437eeaaba3b82deda0dde
Ivan3fonov <ivan3fonov@gmail.com>
2016-09-26 21:10:51 +0300

    Update B_14_Ivaylo_Vasilev.rb

--- h02_quadratic/B_14_Ivaylo_Vasilev.rb
+++ h02_quadratic/B_14_Ivaylo_Vasilev.rb
@@ -5,25 +5,28 @@ c = ARGV[2].to_f
 if (a == 0 && b == 0 && c == 0) 
 	
 	print "#\n"
-end
+        
+        end
 
 if (a == 0 && b != 0) 
   
 	 x1 = c/b.round(2)
-	printf("%g\n", x1)
+	 
+         printf("%g\n", x1)
  
-	elsif a != 0
+ elsif (a != 0)
 		
-	 	D = b*b - 4*a*c
+	 	D = b**2 - 4*a*c
 
 	 	if (D == 0)
 
     		 	x = (-b/2/a).round(2)
-    		 	printf("%g\n", x)	
+    		 	
+                        printf("%g\n", x)	
  
 	 	elsif (D > 0)
       
-				 	x1 = ((-b+Math.sqrt(D))/2/a).round(2)
+			x1 = ((-b+Math.sqrt(D))/2/a).round(2)
      		 	x2 = ((-b-Math.sqrt(D))/2/a).round(2)
 	
 	 	if(x2 < x1) 
@@ -31,8 +34,8 @@ if (a == 0 && b != 0)
 				printf("%g,%g\n",x2 ,x1)
 		
  	 else 
-			 	 printf("%g,%g\n" ,x1 ,x2)
+			 	printf("%g,%g\n",x1 ,x2)
 
-					 end
+				 end
   		end  
  end
