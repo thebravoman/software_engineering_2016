92c1875f0f067ef33c2bf1ec54609ca286c39e04
Willuminat <velislavvelikov2@gmail.com>
2016-09-26 21:44:02 +0300

    Update B_06_Velislav_Velikov.rb

--- h02_quadratic/B_06_Velislav_Velikov.rb
+++ h02_quadratic/B_06_Velislav_Velikov.rb
@@ -1,7 +1,6 @@
-include Math
-a = gets.to_f	#3
-b = gets.to_f	#1
-c = gets.to_f	#-4
+a = ARGV[0].to_f	#3
+b = ARGV[1].to_f	#1
+c = ARGV[2].to_f	#-4
 D = b*b - 4*a*c #3*3-4*(-4)*1
 if(D<0)
 	puts "#"
@@ -13,17 +12,20 @@ if(D>0)
 	x1 = ((b*-1) + Math.sqrt(D)) / (2*a)
     x2 = ((b*-1) - Math.sqrt(D)) / (2*a)
 end
-if(x1<x2)
-	if(x1%1==0 && x2%1==0) 
-		print"#{x1.round(2)},#{x2.round(2)}\n"
-	elseif(x1%1==0)
-		print"#{x1},#{x2.round(2)}\n"
-	elseif(x2%1==0)
-		print"#{x1.round(2)},#{x2}\n"
-	else 
-		print"#{x1},#{x2}\n"
-	end
-	
-else
-	print"#{x2.round(2)},#{x1.round(2)}\n"
+if x1 > x2
+		rev = x1
+		x1 = x2
+		x2 = rev
 end
+if x1 % 1 != 0
+	print sprintf( "%.2f,", x1 )
+else
+	print "#{x1.to_i},"
+end
+if x2 % 1 != 0
+	print sprintf( "%.2f", x2 )
+else
+	print x2.to_i;
+end
+print "\n";
+
