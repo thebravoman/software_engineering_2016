e94d7094142bea17b103db837c1b655bc3fe8a06
6рек <busternr@abv.bg>
2016-09-27 08:50:05 -0700

    Update B_22_Nikolai_Romanov.rb

--- h02_quadratic/B_22_Nikolai_Romanov.rb
+++ h02_quadratic/B_22_Nikolai_Romanov.rb
@@ -1,20 +1,57 @@
-A = ARGV[0].to_f
-B = ARGV[1].to_f
-C = ARGV[2].to_f
-D = B*B - 4*A*C
-if D==0
-	X=-B/2*A
-	puts X.round(0)
+a = Float(ARGV[0])
+b = Float(ARGV[1])
+c = Float(ARGV[2])
+check_if_int = 0
+
+if a == 0 then
+  if b == 0 then
+    if c == 0 then
+      result = "#"
+    else
+      result =  "No solutions."
+    end
+  else
+    result = Float(-c/b).round(2)
+    check_if_int = 1
+  end
 else
-	if D>0
-		X1=((-B-Math.sqrt(D))/2/A).round(0)
-		X2=((-B+Math.sqrt(D))/2/A).round(0)
-		if X1>X2
-                        puts X2.to_s + "," + X1.to_s
-                end
-                if X1<X2
-                        puts X1.to_s + "," + X2.to_s
-                end
-   end
+  d = b*b - 4*a*c
+  if d > 0 then
+    check_if_int = 2
+    result = Array.new
+    x = ( -b + Math.sqrt(d) ) / (2 * a)
+    result[0] = x.round(2)
+    x = ( -b - Math.sqrt(d) ) / (2 * a)
+    result[1] = x.round(2)
+    if result[0] > result[1] then
+      result[0], result[1] = result[1], result[0]
+    end
+  elsif d == 0 then
+    result = ( ( -b ) / (2 * a) ).round(2)
+    check_if_int = 1
+  else
+    result = "No solutions."
+  end
 end
 
+if check_if_int == 1 && result % 1 == 0 then
+  result = Integer(result)
+end
+
+if check_if_int == 2 then
+  if result[0] % 1 == 0 then
+    print String(Integer(result[0])) << ','
+  else
+    print String(result[0]) << ','
+  end
+
+  if result[1] % 1 == 0 then
+    print String(Integer(result[1])) << "\n"
+  else
+    print String(result[1]) << "\n"
+  end
+end
+
+if check_if_int != 2 then
+  puts result
+end
