f46b08331ddda8e3dde8130a26d137ca50ccda28
Михаил Ернандес <mihailernandes@gmail.com>
2016-09-28 13:56:10 +0300

    Create A_22_Mihail_Ernandes.rb

---
+++ c04/A_22_Mihail_Ernandes.rb
@@ -0,0 +1,24 @@
+require 'csv'
+
+def most_watched_video()
+   
+end
+
+def most_viewed_video()
+
+end
+
+def most_views_viewer()
+
+end
+
+def most_viewed_viewer()
+    
+end
+
+data = ARGV[0].to_s
+array = new.Array[]
+
+CSV.foreach(data) do |row|
+    
+        
