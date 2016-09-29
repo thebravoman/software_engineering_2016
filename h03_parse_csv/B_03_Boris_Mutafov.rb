d26f959306727d78ab654c35165c3dd90bb49128
Boris Mutafov <mutafow@gmail.com>
2016-09-29 15:44:46 +0300

    Update B_03_Boris_Mutafov.rb

--- h03_parse_csv/B_03_Boris_Mutafov.rb
+++ h03_parse_csv/B_03_Boris_Mutafov.rb
@@ -1,10 +1,6 @@
 require 'csv'
-data, output, percent_watched,  user_percent_watched, times_watched, user_times_watched  = ARGV[0], ARGV[1].to_i, Hash.new(0.0), Hash.new(0.0), Hash.new(0), Hash.new(0)
+data, output, universal = ARGV[0], ARGV[1].to_i, Array.new(5) {Hash.new(0.0)}
 CSV.foreach(data) do |row|
-	  percent_watched[row[1].to_i]+=row[2].to_f if output == 1
-	  times_watched[row[1].to_i]+=1 if output == 2
-	  user_percent_watched[row[0].to_i]+=row[2].to_f if output == 3
-	  user_times_watched[row[0].to_i]+=1 if output ==4 
+	  universal[output][row[ output == 1 || output == 2 ? 1 : 0 ].to_i]+= output == 1 || output == 3 ? row[2].to_f : output == 4 ? 1 : output == 2 ? 1 : 0
 end
-result = (percent_watched.max_by{|k,v| v} if output == 1 ) || (times_watched.max_by{|k,v| v} if output === 2 ) || (user_percent_watched.max_by{|k,v| v} if output == 3) || (user_times_watched.max_by{|k,v| v} if output == 4 ) || "Error"
-output == 1 ? printf("%i,%.2f", result[0], result[1]) : output == 3 ? printf("%i,%.1f", result[0], result[1]) : printf("%i,%i", result[0], result[1])
+puts String(universal[output].max_by{|k,v| v}[0].to_i) << ',' << String(output == 1 ? universal[output].max_by{|k,v| v}[1].round(2) : output == 2 || output == 4 ? universal[output].max_by{|k,v| v}[1].to_i : universal[output].max_by{|k,v| v}[1])
