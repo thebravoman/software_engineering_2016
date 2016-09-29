68141c7b661e5b2a1ddec21774eedb7b3806d712
Boris Mutafov <mutafow@gmail.com>
2016-09-29 21:17:10 +0300

    Update B_03_Boris_Mutafov.rb

--- h03_parse_csv/B_03_Boris_Mutafov.rb
+++ h03_parse_csv/B_03_Boris_Mutafov.rb
@@ -3,4 +3,4 @@ data, output, full_data = ARGV[0], ARGV[1].to_i, Array.new(5) {Hash.new(0.0)}
 CSV.foreach(data) do |row|  
 	full_data[output][row[ output < 3 ? 1 : 0 ].to_i]+= output % 2 == 1 ? row[2].to_f : 1
 end
-printf( '%i,%g'"\n", *full_data[output].max_by{|k,v| v} )
+printf '%i,%g'"\n", *full_data[output].max_by{|k,v| v}
