442ab2b55348482920219d3697b07ff21065a3fc
NIkola Tzotchev (Никола Цочев) <ntzotchev@gmail.com>
2016-09-29 21:21:30 +0300

    Update B_21_Nikola_Tzotchev.rb

--- h03_parse_csv/B_21_Nikola_Tzotchev.rb
+++ h03_parse_csv/B_21_Nikola_Tzotchev.rb
@@ -1,16 +1,6 @@
 require 'csv'
-data, output, percent_watched,  user_percent_watched, times_watched, user_times_watched  = ARGV[0], ARGV[1], Hash.new(0.0), Hash.new(0.0), Hash.new(0), Hash.new(0)
-CSV.foreach(data) do |row|
-	case output.to_i
-	  when 1 then percent_watched[row[1].to_i]+=row[2].to_f
-	  when 2 then times_watched[row[1].to_i]+=1
-	  when 3 then user_percent_watched[row[0].to_i]+=row[2].to_f
-	  when 4 then user_times_watched[row[0].to_i]+=1
-	end
+data, output, full_data = ARGV[0], ARGV[1].to_i, Array.new(5) {Hash.new(0.0)}
+CSV.foreach(data) do |row|  
+	full_data[output][row[ output < 3 ? 1 : 0 ].to_i]+= output % 2 == 1 ? row[2].to_f : 1
 end
-case output.to_i
-	when 1 then printf('%i,%.2f' "\n", *percent_watched.max_by{|k,v| v})
-	when 2 then printf('%i,%i'   "\n", *times_watched.max_by{|k,v| v})
-	when 3 then printf('%i,%.2f' "\n", *user_percent_watched.max_by{|k,v| v})
-	when 4 then printf('%i,%i'   "\n", *user_times_watched.max_by{|k,v| v})
-end
+printf '%i,%g'"\n", *full_data[output].max_by{|k,v| v}
