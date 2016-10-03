b0773db62037a7f58ff1329b921cf6aca624862f
ivanganchev <ivan_ganchev45@abv.bg>
2016-10-03 23:39:36 +0300

    hw3

---
+++ h03_parse_csv/A_12_Ivan_Ganchev.rb
@@ -0,0 +1,79 @@
+require 'csv'
+
+$videos = Hash.new
+
+def most_watched_by_percent(row)
+	video = row[1]
+	percent = row[2].to_f
+	
+	if $videos[video] == nil
+		$videos[video] = percent
+		
+	else
+		$videos[video] += percent
+		
+	end
+	 
+end
+
+
+def most_watched_by_open(row)
+
+	video = row[1].to_i
+	
+	if $videos[video] == nil
+		$videos[video] = 1		
+	else
+		$videos[video] += 1
+		
+	end
+end
+def most_watched_by_user_percent(row)
+	
+	user = row[0]
+	percent = row[2].to_f
+
+	if $videos[user] == nil
+		$videos[user] = percent	
+	else
+		$videos[user] += percent
+		
+	end
+end
+
+def user_who_watched_most_videos(row)
+	
+	user = row[0]
+	
+
+	if $videos[user] == nil
+		$videos[user] = 1
+	else
+		$videos[user] += 1
+		
+	end
+end
+
+num = ARGV[1].to_i
+
+CSV.foreach('data.csv') do |row|
+	if num == 1
+		most_watched_by_percent row 
+	elsif num == 2
+		most_watched_by_open row
+	elsif num == 3
+		most_watched_by_user_percent row
+	elsif num == 4
+		user_who_watched_most_videos row
+	end
+end
+
+if num == 1
+	puts  "#{$videos.max_by{|k,v| v}[0]},#{$videos.max_by{|k,v| v}[1].round(2)}"
+elsif num == 2
+	puts  "#{$videos.max_by{|k,v| v}[0]},#{$videos.max_by{|k,v| v}[1]}"
+elsif num == 3
+	puts  "#{$videos.max_by{|k,v| v}[0]},#{$videos.max_by{|k,v| v}[1].round(2)}"
+elsif num == 4
+	puts  "#{$videos.max_by{|k,v| v}[0]},#{$videos.max_by{|k,v| v}[1]}"
+end
