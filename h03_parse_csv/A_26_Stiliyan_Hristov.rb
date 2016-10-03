852d1770c5bcbef1a7a392967b2af80fd960853a
Stiliyan Hristov <stili.hristov@gmail.com>
2016-10-03 18:36:48 +0300

    Adding H03 parsing csv

---
+++ h03_parse_csv/A_26_Stiliyan_Hristov.rb
@@ -0,0 +1,150 @@
+
+file1 = ARGV[0].to_s
+method = ARGV[1].to_i
+
+require 'csv'
+arr = Array.new
+i = 0
+
+CSV.foreach(File.path(file1)) do |row|
+	arr[i] = row
+	i+=1
+end
+
+
+
+i = 0 
+ 
+hash = Hash.new
+
+hash.default = 0
+
+if method == 1
+
+
+	arr.each do |video|
+	
+	hash[video[1].to_i] =  hash[video[1].to_i] + video[2].to_f
+	
+	end 
+
+	hash = hash.sort_by {|k,v| v}.reverse
+
+	print "#{hash[0][0]},"
+	
+	if hash[0][1] % 1 == 0 
+		puts hash[0][1].to_i
+	else
+		puts hash[0][1].round(2)
+	end
+
+
+	if hash[0][1] == hash[1][1]
+		print "#{hash[1][0]},"
+	
+	if hash[1][1] % 1 == 0 
+		puts hash[1][1].to_i
+	else
+		puts hash[1][1].round(2)
+	end
+
+	end
+
+end
+
+if method == 3
+	
+	arr.each do |video|
+	
+	hash[video[0].to_i] =  hash[video[0].to_i] + video[2].to_f
+	
+	end 
+
+	hash = hash.sort_by {|k,v| v}.reverse
+
+	#puts hash.inspect
+		
+
+	print "#{hash[0][0]},"
+	
+	if hash[0][1] % 1 == 0 
+		puts hash[0][1].to_i
+	else
+		puts hash[0][1].round(2)
+	end
+
+
+	if hash[0][1] == hash[1][1]
+		print "#{hash[1][0]},"
+	
+	if hash[1][1] % 1 == 0 
+		puts hash[1][1].to_i
+	else
+		puts hash[1][1].round(2)
+	end
+
+	end	
+end
+
+if method == 2
+
+		arr.each do|video|
+	
+
+	hash[video[1].to_i] =  hash[video[1].to_i] + 1  
+		end 
+		
+		hash = hash.sort_by {|k,v| v}.reverse
+		print "#{hash[0][0]},"
+	
+	if hash[0][1] % 1 == 0 
+		puts hash[0][1].to_i
+	else
+		puts hash[0][1].round(2)
+	end
+
+
+	if hash[0][1] == hash[1][1]
+		print "#{hash[1][0]},"
+	
+	if hash[1][1] % 1 == 0 
+		puts hash[1][1].to_i
+	else
+		puts hash[1][1].round(2)
+	end
+
+	end	
+
+end
+
+
+if method == 4
+		arr.each do|video|
+	
+
+	hash[video[0].to_i] =  hash[video[0].to_i] + 1  
+		end 
+		
+		hash = hash.sort_by {|k,v| v}.reverse
+		print "#{hash[0][0]},"
+	
+	if hash[0][1] % 1 == 0 
+		puts hash[0][1].to_i
+	else
+		puts hash[0][1]
+	end
+
+
+	if hash[0][1] == hash[1][1]
+		print "#{hash[1][0]},"
+	
+	if hash[1][1] % 1 == 0 
+		puts hash[1][1].to_i
+	else
+		puts hash[1][1]
+	end
+
+	end	
+
+end
+
