652de98ebe30bd712eb2eb2eeea7fb603fc6d9d0
Yellobelly <alexstoyanof@gmail.com>
2016-10-10 23:48:17 +0300

    h03 sort of working :)

--- h03_parse_csv/A_01_Alexander_Stoyanov.rb
+++ h03_parse_csv/A_01_Alexander_Stoyanov.rb
@@ -10,83 +10,112 @@ copy = CSV.read(file)
 
 content = Hash.new
 
-def most_viewed(content)
 
+
+
+def most_viewed(content)
+result = Hash.new
 	
 			content.each do |key, value|
-			 
-			  value.each do |k,v|
-			    
-			  end
-			end
-	
+ 			  value.each do |k,v|
+				
+
+				if result[k.to_i] == nil
+				 result[k.to_i] = v.to_f
+				else
+				 result[k.to_i] =  result[k.to_i] + v.to_f	
+			
+				end 
+ 			  end
+			end 
+	puts "#{result.max_by{|k,v| v}}"
 	
 	end
+
+
+
+def most_watched(content)
+result = Hash.new
+		
+		content.each do |key, value|
+				  value.each do |k,v|
+						if result[k.to_i] == nil
+						 result[k.to_i] = 1
+						else
+						 result[k.to_i] =  result[k.to_i] + 1
+			
+						end 
+ 			  		end
+			end
+		
+	puts "#{result.max_by{|k,v| v}}"
+		
+
+	end
+
+
+def most_zombified(content)
+result = Hash.new
 	
+			content.each do |key, value|
+ 			  value.each do |k,v|
+				
+
+				if result[key.to_i] == nil
+				 result[key.to_i] = v.to_f.round(2)
+				else
+				 result[key.to_i] =  result[key.to_i] + v.to_f.round(2)	
+			
+				end 
+ 			  end
+			end
+	puts "#{result.max_by{|k,v| v}}"
+	
+end
+
+def most_videos_watched(content)
+result = Hash.new
+		
+		content.each do |key, value|
+				  value.each do |k,v|
+						if result[key.to_i] == nil
+						 result[key.to_i] = 1
+						else
+						 result[key.to_i] =  result[key.to_i] + 1
+			
+						end 
+ 			  		end
+			end
+		
+	puts "#{result.max_by{|k,v| v}}"
+		
+
+	end
+
+
+
 
 
 copy.each do |row|
+		if content[row[0].to_i] == nil
 		content[row[0].to_i] = Hash.new()
-		content[row[0].to_i][row[1].to_i] = row[2].to_i 
-end
-
-most_viewed(content)
-=begin
-require 'csv'
-
-
-
-
-file = ARGV[0].to_s
-task = ARGV[1].to_i
-
-copy = CSV.read(file)
-
-
-content = Hash.new
-
-def most_viewed(content)
-
-	puts content
-	puts content.size
-	i=1
-	m=0
-	f=0
-	g=0	
-		while i	< content.size do 
-			k=0
-			while k < content.size do 
-				puts content[i]
-					while f	< content.size do 
-						g=0
-						while g < content.size do 
-							if content[i][f] == content[k][g]
-								puts content[i][f]
-								
-							end
-							g=g+1
-						end
-
-
-					f=f+1
-					end
-					
-				k=k+1
-			end
-
-
-		i=i+1
 		end
-	
-	puts m
-	end
-	
-
-
-copy.each do |row|
-		content[row[0].to_i] = Hash.new()
-		content[row[0].to_i][row[1].to_i] = row[2].to_i 
+		content[row[0].to_i][row[1].to_i] = row[2].to_f
 end
 
+if task == 1
 most_viewed(content)
-=end
+end
+
+if task == 2
+most_watched(content)
+end
+
+if task == 3
+most_zombified(content)
+end
+
+if task == 4
+most_videos_watched(content)
+end 
+
