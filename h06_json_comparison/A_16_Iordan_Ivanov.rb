a2427a98aac2d37da360841a8196fc65ec41f679
iordanivanov <iiordan.ivanov@gmail.com>
2016-10-17 23:12:35 +0300

    Update A_16_Iordan_Ivanov.rb

--- h06_json_comparison/A_16_Iordan_Ivanov.rb
+++ h06_json_comparison/A_16_Iordan_Ivanov.rb
@@ -1,9 +1,5 @@
 require 'json'
-
-file1 = JSON.parse File.read ARGV[0]
-file2 = JSON.parse File.read ARGV[1]
-
-if file1 == file2
+if JSON.parse(File.read(ARGV[0])) == JSON.parse(File.read(ARGV[1]))
   p 1
 else
   p 0
