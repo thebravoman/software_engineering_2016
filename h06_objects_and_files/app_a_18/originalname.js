28e2ef66ae1a5d7648e66ae63d227cd8fc135069
LilianVachkov <lilvachkov@gmail.com>
2016-10-17 20:18:00 +0300

    Homeworkkk

--- h06_objects_and_files/app_a_18/originalname.js
+++ h06_objects_and_files/app_a_18/originalname.js
@@ -5,19 +5,23 @@ fs.readFile('./val.js', 'utf8', function (err, data) {
   obj = JSON.parse(data);
 
 	var result = {x1:undefined, x2:undefined, D:undefined};
-	result.D = Math.pow(obj.b, 2) - 4*obj.a*obj.c;
 
 	if(!obj.a && obj.b)
-		result.x1 = result.x2 = (-obj.c/obj.b).toFixed(2);
-	else if(!obj.b) {
-
+		if((result.x1 = result.x2 = (-obj.c/obj.b))%1 != 0)
+			result.x1 = result.x2 = result.x1.toFixed(2);
+	if(!obj.b && obj.a && obj.c) {
+		if((result.x1 = result.x2 = (Math.sqrt(-obj.c/obj.a)).toFixed(2)) == null)
+			result.x1 = result.x2 = undefined;
+		
 	}
-	if(result.D >= 0) {
-		if((result.x1 = (-obj.b + Math.sqrt(result.D))/(2*obj.a))%1 != 0)
-			result.x1 = x1.toFixed(2);
-		if((result.x2 = (-obj.b - Math.sqrt(result.D))/(2*obj.a))%1 != 0)
-			result.x2 = x2.toFixed(2);
+	if(obj.a && obj.b && obj.c) {
+		result.D = Math.pow(obj.b, 2) - 4*obj.a*obj.c;
+		if(result.D >= 0) {
+			if((result.x1 = (-obj.b + Math.sqrt(result.D))/(2*obj.a))%1 != 0)
+				result.x1 = result.x1.toFixed(2);
+			if((result.x2 = (-obj.b - Math.sqrt(result.D))/(2*obj.a))%1 != 0)
+				result.x2 = result.x2.toFixed(2);
+		}
 	}
-
 	console.log(JSON.stringify(result, null, 1	));
 });
