b7300397b59d39905d898db567cdb9957ac98141
IvoTod <ivo.tod.todorov@gmail.com>
2016-10-17 17:22:46 +0300

    Fixed code printing strings instead of numbers

--- h06_objects_and_files/app_a_14/homework.js
+++ h06_objects_and_files/app_a_14/homework.js
@@ -15,7 +15,7 @@ fs.readFile("./input.json", "utf-8", function(err, data){
 		}
 		else if(b!=0){
 			let x = -c/b;
-			let output = {"x1":(x % 1 != 0 ? x.toFixed(1) : x), "x2":(x % 1 != 0 ? x.toFixed(1) : x), "D":undefined}
+			let output = {"x1":Number((x % 1 != 0 ? x.toFixed(2) : x)), "x2":Number((x % 1 != 0 ? x.toFixed(2) : x)), "D":undefined}
 			console.log(JSON.stringify(output, null, 2));
 		}
 	}
@@ -26,13 +26,13 @@ fs.readFile("./input.json", "utf-8", function(err, data){
 			let x1, x2;
 			x1 = (-args.b + Math.sqrt(d))/(2*args.a);
 			x2 = (-args.b - Math.sqrt(d))/(2*args.a);
-			let output = {"x1":(x1 % 1 != 0 ? x1.toFixed(1) : x1), "x2":(x2 % 1 != 0 ? x2.toFixed(2) : x2), "D":d}
+			let output = {"x1":Number((x1 % 1 != 0 ? x1.toFixed(2) : x1)), "x2":Number((x2 % 1 != 0 ? x2.toFixed(2) : x2)), "D":Number((d % 1 != 0 ? d.toFixed(2) : d))}
 			console.log(JSON.stringify(output, null, 2));
 		}
 		else if (d==0){
 			let x;
 			x = -b/(2*a);
-			let output = {"x1":(x % 1 != 0 ? x.toFixed(1) : x), "x2":(x % 1 != 0 ? x.toFixed(1) : x), "d":d};
+			let output = {"x1":Number((x % 1 != 0 ? x.toFixed(2) : x)), "x2":Number((x % 1 != 0 ? x.toFixed(2) : x)), "d":Number((d % 1 != 0 ? d.toFixed(2) : d))};
 			console.log(JSON.stringify(output, null, 2));
 		}
 	}
