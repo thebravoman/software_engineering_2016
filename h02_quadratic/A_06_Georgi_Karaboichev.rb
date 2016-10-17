2525c3ee184e555f06325b988ea88ccc275d098f
prot3ct <djorkaevhell@abv.bg>
2016-10-17 18:54:09 +0300

    Refactor HW 02

--- h02_quadratic/A_06_Georgi_Karaboichev.rb
+++ h02_quadratic/A_06_Georgi_Karaboichev.rb
@@ -2,17 +2,23 @@ a = ARGV[0].to_f
 b = ARGV[1].to_f
 c = ARGV[2].to_f
 
+def roundNumberIfNeeded(number)
+	if(number % 1 == 0)
+		number = number.to_i
+	else
+		number = (number).round(2)
+	end
+
+	return number
+end
+
 squares = []
 if (a == 0)
 	if (b == 0 && c == 0)
 		puts "#"
 	else
 		squares[0] = -(c / a)
-		if(squares[0] % 1 == 0)
-			squares[0] = squares[0].to_i
-		else
-			squares[0] = squares[0].round(2)
-		end
+		squares[0] = roundNumberIfNeeded(squares[0])
 	end
 else
 	flag = false
@@ -22,25 +28,16 @@ else
 	if (discriminant > 0)
 		squares[0] = (-b + Math.sqrt(discriminant)) / (2 * a)
 		squares[1] = (-b - Math.sqrt(discriminant)) / (2 * a)
-		flag = true
 	elsif (discriminant == 0)
 		squares[0] = -b / (2 * a)
 	else
 		abort
 	end
 
-	if(squares[0] % 1 == 0)
-		squares[0] = squares[0].to_i
-	else
-		squares[0] = squares[0].round(2)
+	for i in 0..squares.length - 1
+		squares[i] = roundNumberIfNeeded(squares[i])
 	end
-	if(flag)
-		if(squares[1] % 1 == 0)
-			squares[1] = squares[1].to_i	
-		else
-			squares[1] = squares[1].round(2)
-		end
-	end
+
 	if(discriminant >= 0)
 		puts squares.sort().join(",")
 	end
