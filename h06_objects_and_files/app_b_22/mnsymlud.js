8a90932ce667ee3544200e1738f7e8c623b8e582
6рек <busternr@abv.bg>
2016-10-17 20:07:17 +0300

    Update mnsymlud.js

--- h06_objects_and_files/app_b_22/mnsymlud.js
+++ h06_objects_and_files/app_b_22/mnsymlud.js
@@ -1,10 +1,6 @@
 var fs = require("fs");
 var file = "input.json";
-<<<<<<< HEAD
 var packageJSON = JSON.parse(fs.readFileSync(file));
-=======
-var packageJSON = packageJSON = JSON.parse(fs.readFileSync(file));
->>>>>>> c68384ec23b96a307dd62eb191c06bd9d6c2c2ea
 var A = packageJSON.a;
 var B = packageJSON.b;
 var C = packageJSON.c;
@@ -13,7 +9,7 @@ var x1 = 0;
 var x2 = 0
 D = B * B  - (4 * A * C );
 if(D < 0) console.log("");
-else if (D > 0)
+else if (D < 0)
 {
 	x1 = (-B + (Math.sqrt(D))) / (2 * A);
 	x2 = (-B - (Math.sqrt(D))) / (2 * A);
