bc5491f03d127bca7c44afdfeebd1d2309b02e67
Slavkata <slavkirilov00@gmail.com>
2016-10-18 19:48:49 +0300

    Update B_24_Slav_Kirilov.rb

--- h03_parse_csv/B_24_Slav_Kirilov.rb
+++ h03_parse_csv/B_24_Slav_Kirilov.rb
@@ -1,11 +1,19 @@
 require 'csv'
-data, output, percent_watched,  user_percent_watched, times_watched, user_times_watched  = ARGV[0], ARGV[1], Hash.new(0.0), Hash.new(0.0), Hash.new(0), Hash.new(0)
+data = ARGV[0]
+output = ARGV[1]
+percent_watched = Hash.new(0.0)
+user_percent_watched = Hash.new(0.0)
+times_watched = Hash.new(0.0)
+user_times_watched = Hash.new(0.0)
 CSV.foreach(data) do |row|
+	vid = row [ 0 ].to_i
+	id = row [ 1 ].to_i
+	percents = row [ 2 ].to_f
 	case output.to_i
-	  when 1 then percent_watched[row[1].to_i]+=row[2].to_f
-	  when 2 then times_watched[row[1].to_i]+=1
-	  when 3 then user_percent_watched[row[0].to_i]+=row[2].to_f
-	  when 4 then user_times_watched[row[0].to_i]+=1
+	  when 1 then percent_watched [ id ] += percents
+	  when 2 then times_watched [ id ] += 1
+	  when 3 then user_percent_watched [ vid ] += percents
+	  when 4 then user_times_watched [ vid ] += 1
 	end
 end
 case output.to_i
