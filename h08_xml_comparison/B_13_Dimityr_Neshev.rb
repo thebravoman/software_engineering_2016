68856e551c5f2f0e3e21a5614663f5e2db5646c7
Dimitur Neshev <d.neshev99@gmail.com>
2016-10-22 19:05:10 +0300

    Update B_13_Dimityr_Neshev.rb

--- h08_xml_comparison/B_13_Dimityr_Neshev.rb
+++ h08_xml_comparison/B_13_Dimityr_Neshev.rb
@@ -2,11 +2,8 @@ require 'rubygems'
 require 'json'
 require 'crack'
 
-
-
 file_1 = File.read(ARGV[0])
 file_2 = File.read(ARGV[1])
-
 
 file_xml=Crack::XML.parse(file_1)
 file_json=JSON.parse(file_2)
