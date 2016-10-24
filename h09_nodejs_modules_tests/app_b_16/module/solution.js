c9e7b31e5328ed869d3b3f20929cc27752a19f4e
Ivan3fonov <ivan3fonov@gmail.com>
2016-10-24 23:23:19 +0300

    Update solution.js

--- h09_nodejs_modules_tests/app_b_16/module/solution.js
+++ h09_nodejs_modules_tests/app_b_16/module/solution.js
@@ -1,43 +1,24 @@
-function is_int(num) {
-	if (num % 1 != 0){
-		num=num.toFixed(2)/1;
-	}
-	return num;
-}
-
 exports.solve = function(a, b, c){
-	var result ={};
-	if(a==0 && b==0 && c==0){
-		result.x = "Any";
-		
-	}
-	 if( a==0 && b!=0){
-		result.x = -c/b ;
-		is_int(result.x);
-		
-	}
-	else if ( b != 0 ) {
-
-		result.D = b*b -4*a*c ;
-		is_int(result.D);
-		
-		if( result.D > 0) {
-			result.x1 = (-b + Math.sqrt(result.D))/(2*a);
-			result.x2 = (-b - Math.sqrt(result.D))/(2*a);
-			is_int(result.x1);
-			is_int(result.x2);
-		}
-		else  if (result.D == 0) {
-			result.x = (-b)/(2*a);
-			is_int(result.x);
-			
-		}
-		else {
-			result.x1="NaN";
-			result.x2="NaN";	
-		}
-	}
-
-	
-	 console.log(JSON.stringify(result, null, 2));
+    var result ={};
+    if(a==0 && b==0 && c==0){
+        result.x = "Any";
+    }
+    if( a==0 && b!=0){
+        result.x = (-c/b).toFixed(2);
+    }
+    else if ( b != 0 ) {
+        var D = b*b -4*a*c;
+        if(D > 0) {
+            result.x1 = (-b + Math.sqrt(D))/(2*a).toFixed(2);
+            result.x2 = (-b - Math.sqrt(D))/(2*a).toFixed(2);
+        }
+        else  if (D == 0) {
+            result.x = (-b)/(2*a).toFixed(2);
+        }
+        else {
+            result.x1="NaN";
+            result.x2="NaN";   
+        }
+    }
+     console.log(JSON.stringify(result, null, 2));
 }
