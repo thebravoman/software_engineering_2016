c7e0d8315cc3debd4c7ab95501454f89f907c819
NIkola Tzotchev (Никола Цочев) <ntzotchev@gmail.com>
2016-10-24 19:27:07 +0300

    Update test.js

--- h09_nodejs_modules_tests/app_b_21/test/test.js
+++ h09_nodejs_modules_tests/app_b_21/test/test.js
@@ -5,7 +5,7 @@ var module1 = require('../modules/func.js');
 var fs = require('fs');
 exports.test_D0 = function(test)
 {
-	var check = "imput-1.json";
+	var check = "input-1.json";
 	var  Expect = JSON.parse(fs.readFileSync(check));
 	test.deepEqual( { x: -1 } , module1.solve(Expect.a,Expect.b,Expect.c));
 	test.done();
@@ -13,14 +13,14 @@ exports.test_D0 = function(test)
 
 exports.test_real = function(test)
 {
-	var check = "imput-2.json";
+	var check = "input-2.json";
 	var  Expect = JSON.parse(fs.readFileSync(check));
 	test.deepEqual({ x1: 1, x2: -0.5} , module1.solve(Expect.a, Expect.b, Expect.c));
 	test.done();
 };
 exports.test_NaN = function(test)
 {
-	var check = "imput-nan.json";
+	var check = "input-nan.json";
 	var checker_for_NaN = {};
 	var Expect = JSON.parse(fs.readFileSync(check));
 	checker_for_NaN = module1.solve(Expect.a, Expect.b, Expect.c);
@@ -29,7 +29,7 @@ exports.test_NaN = function(test)
 };
 exports.test_any = function(test)
 {
-	var check = "imput-any.json";
+	var check = "input-any.json";
 	var Expect = JSON.parse(fs.readFileSync(check));
 	test.deepEqual( { x: 'ANY' } , module1.solve(Expect.a, Expect.b, Expect.c));
 	test.done();		
