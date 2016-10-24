e6cdea5489561b88fe99ae9cc271a696598954c2
Verbo1806 <sasho_ltd@abv.bg>
2016-10-24 21:21:17 +0300

    Sagen Sie Nein zu Rassismus

--- h09_nodejs_modules_tests/app_b_01/modules/modules.js
+++ h09_nodejs_modules_tests/app_b_01/modules/modules.js
@@ -9,6 +9,10 @@ if (a == 0 && b == 0 && c == 0) {
 
 if (a == 0 && b != 0 && c != 0) {
 		    output.x = ((c - 2*c) / b);
+	
+		if (typeof output.x !== 'string' ) {
+       		output.x = (output.x).toFixed(2)/1;
+    }
 }
 
 if (a != 0 && b != 0 && c != 0) {
@@ -20,11 +24,21 @@ if (a != 0 && b != 0 && c != 0) {
         output.x1 = ((b*-1) + Math.sqrt(d)) / (2*a);
     
         output.x2 = ((b*-1) - Math.sqrt(d)) / (2*a);
+	    
+	if (typeof output.x1 !== 'string' ) {
+        output.x1 = (output.x1).toFixed(2)/1;
+    }
+    
+    if (typeof output.x2 !== 'string' ) {
+        output.x2 = (output.x2).toFixed(2)/1;
+    }
 	}
 		 	 
 	if (d == 0) {
     
         output.x = (b*-1) / (2*a);
+	if (typeof output.x !== 'string' ) {
+       	output.x = (output.x).toFixed(2)/1;
     
     }
     
@@ -33,14 +47,6 @@ if (a != 0 && b != 0 && c != 0) {
         output.x2 = "NaN";  
     }
 }
-
-    if (typeof output.x1 !== 'string' ) {
-        output.x1 = (output.x1).toFixed(2)/1;
-    }
-    
-    if (typeof output.x2 !== 'string' ) {
-        output.x2 = (output.x2).toFixed(2)/1;
-    }
 
  return output;
 };
