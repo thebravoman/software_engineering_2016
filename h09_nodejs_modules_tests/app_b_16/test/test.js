591f73995d6b563b14aa849bee3a81f151b49556
Ivan3fonov <ivan3fonov@gmail.com>
2016-10-24 23:21:39 +0300

    Update test.js

--- h09_nodejs_modules_tests/app_b_16/test/test.js
+++ h09_nodejs_modules_tests/app_b_16/test/test.js
@@ -1,45 +1,49 @@
 var module = require('../module/solution.js');
-
-
-	exports.test_none = function(test)
-	{
-		var input = require('./input_files/input_nan.json');
-		test.equal(({
-		    "x1": "NaN",
-		    "x2": "NaN"
-			}).to_json, module.solve(input.a, input.b, input.c));
-		    
-		test.done();
-	};
-
-	exports.test_any = function(test)
-	{
-
-		var input = require('./input_files/input_any.json');
-		test.equal(({"x": "Any"}).to_json, module.solve(input.a, input.b, input.c));
-		
-		test.done();
-	};
-
-	exports.test_D0 = function(test)
-	{
-		var input = require('./input_files/input-1.json');
-
-	    test.equal(({
-	  	"x": -1
-		}).to_json, module.solve(input.a, input.b, input.c));
-
-	test.done();
-	};
-	
-	exports.test_DGreaterThan0 = function(test)
-	{
-		var input = require('./input_files/input-2.json');
-
-	    test.equal(({
-	    "x1": -0.2,
-	    "x2": -1
-		}).to_json, module.solve(input.a, input.b, input.c));
-	    
-	    test.done();
-	};
+var fs = require('fs');
+var json = new Object;
+var input_nan = fs.readFileSync('./input-nan.json');
+var input_any = fs.readFileSync('./input-any.json');
+var input_1_root = fs.readFileSync('./input-1.json');
+var input_2_roots = fs.readFileSync('./input-2.json');
+    exports.test_nan = function(test)
+    {
+        var input = JSON.parse(input_nan);
+        json.x1 = "NaN";
+        json.x2 = "NaN";
+        let a = input.a;
+        let b = input.b;
+        let c = input.c;
+        test.equal(json.to_json, module.solve(a,b,c));
+        test.done();
+    };
+    exports.test_any = function(test)
+    {
+        var input = JSON.parse(input_any);
+        let a = input.a;
+        let b = input.b;
+        let c = input.c;
+        json.x = "Any";
+        test.equal(json.to_json, module.solve(a,b,c));
+        test.done();
+    };
+    exports.test_1_root = function(test)
+    {
+        var input = JSON.parse(input_1_root);
+        let a = input.a;
+        let b = input.b;
+        let c = input.c;
+        json.x = -2;
+        test.equal(json.to_json, module.solve(a,b,c));
+        test.done();
+    };
+    exports.test_2_roots = function(test)
+    {
+        var input = JSON.parse(input_2_roots);
+        let a = input.a;
+        let b = input.b;
+        let c = input.c;
+        json.x1 = -0.2;
+        json.x2 = -1;
+        test.equal(json.to_json, module.solve(a,b,c));
+        test.done();
+    };
