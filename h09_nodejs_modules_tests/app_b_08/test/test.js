195abaa5b054cae5140d8afbea0d009040c4d544
Cepe6 <vmladenov22@gmail.com>
2016-10-24 20:22:25 +0300

    Update hw

--- h09_nodejs_modules_tests/app_b_08/test/test.js
+++ h09_nodejs_modules_tests/app_b_08/test/test.js
@@ -3,45 +3,29 @@ var fs = require('fs');
 
 
 exports.test_none = function(test) {
-	var data = JSON.parse(fs.readFile('./none.json'));
-	var returnExpected = {
-			"x1":"NaN",
-			"x2":"NaN",
-			"D":0
-	};
+	var data = require('./none.json');
 	
-	test.equals(returnExpected, equation.solve(data.a, data.b, data.c));
+	test.equals(JSON.stringify({"x1":"NaN","x2":"NaN"}), JSON.stringify(equation.solve(data.a, data.b, data.c)));
 	test.done();
 };
 
 exports.test_any = function(test) {
-	var data = JSON.parse(fs.readFile('./any.json'));
-	var returnExpected = {
-			"x1":"Any",
-			"x2":"Any",
-			"D":0
-	};
+	var data = require('./any.json');
 	
-	test.equals(returnExpected, equation.solve(data.a, data.b, data.c));
+	test.equals(JSON.stringify({"x":"Any"}), JSON.stringify(equation.solve(data.a, data.b, data.c)));
+	test.done();
 };
 
 exports.test_D0 = function(test) {
-	var data = JSON.parse(fs.readFile('./input-1.json'));
-	var returnExpected = {
-			"x": 2,
-			"D": 0
-	};
+	var data = require('./input-1.json');
 	
-	test.equals(returnExpected, equation.solve(data.a, data.b, data.c));
+	test.equals(JSON.stringify({"x":2}), JSON.stringify(equation.solve(data.a, data.b, data.c)));
+	test.done();
 };
 
 exports.test_DGreaterThan0 = function(test) {
-	var data = JSON.parse(fs.readFile('./input-2.json'));
-	var returnExpected = {
-			"x1":4,
-			"x2":-1,
-			"D":25
-	};
-	
-	test.equals(returnExpected, equation.solve(data.a, data.b, data.c));
+	var data = require('./input-2.json');
+
+	test.equals(JSON.stringify({"x1":4,"x2":-1}), JSON.stringify(equation.solve(data.a, data.b, data.c)));
+	test.done();
 };
