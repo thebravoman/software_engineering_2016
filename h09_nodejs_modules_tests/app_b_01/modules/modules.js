a557668fd148d2fec0f6f6cb259a296665ddb3e4
Verbo1806 <sasho_ltd@abv.bg>
2016-10-24 21:23:55 +0300

    Sagen Sie Nein zu Rassismus

--- h09_nodejs_modules_tests/app_b_01/modules/modules.js
+++ h09_nodejs_modules_tests/app_b_01/modules/modules.js
@@ -9,10 +9,6 @@ if (a == 0 && b == 0 && c == 0) {
 
 if (a == 0 && b != 0 && c != 0) {
 		    output.x = ((c - 2*c) / b);
-	
-		if (typeof output.x !== 'string' ) {
-       		output.x = (output.x).toFixed(2)/1;
-    }
 }
 
 if (a != 0 && b != 0 && c != 0) {
@@ -25,22 +21,11 @@ if (a != 0 && b != 0 && c != 0) {
     
         output.x2 = ((b*-1) - Math.sqrt(d)) / (2*a);
 	    
-	if (typeof output.x1 !== 'string' ) {
-        output.x1 = (output.x1).toFixed(2)/1;
-    }
-    
-    if (typeof output.x2 !== 'string' ) {
-        output.x2 = (output.x2).toFixed(2)/1;
-    }
 	}
 		 	 
 	if (d == 0) {
     
         output.x = (b*-1) / (2*a);
-	if (typeof output.x !== 'string' ) {
-       	output.x = (output.x).toFixed(2)/1;
-    
-    }
     
     if (d < 0) {
         output.x1 = "NaN";
