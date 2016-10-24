fc44c8cb1ae7b060e644657cd6ec56f71b192cef
VelislavYotov <slaviy999@abv.bg>
2016-10-24 23:42:28 +0300

    Update math.js

--- h09_nodejs_modules_tests/app_a_05/modules/math.js
+++ h09_nodejs_modules_tests/app_a_05/modules/math.js
@@ -11,14 +11,20 @@ exports.math = function (a,b,c)
 		return output;		
 		}
 	root=-c/b;
-	root=root.toFixed(2)/1;
+	if(root%1!=0)
+		{
+		root=root.toFixed(2)/1;
+		}	
 	output={x:root};
 	return output;
 	}
 	else if(D==0)
 	{
 	root=-b/2*a;
-	root=root.toFixed(2)/1;
+	if(root%1!=0)
+		{
+		root=root.toFixed(2)/1;
+		}	
 	output={x:root};	
 	return output;	
 	}
@@ -26,6 +32,14 @@ exports.math = function (a,b,c)
 	{	
 	root_f=(-b+Math.sqrt(D))/2/a;
 	root_s=(-b-Math.sqrt(D))/2/a;
+		if(root_f%1!=0)
+		{
+			root_f=root_f.toFixed(2)/1;
+		}	
+		if(root_s%1!=0)
+		{
+			root_s=root_s.toFixed(2)/1;
+		}	
 	output={x1:root_f,x2:root_s};
 	return output;	
 	}
