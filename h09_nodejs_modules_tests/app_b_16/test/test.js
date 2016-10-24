0b6d566e688dd908739dc9b9b308ca0f253cad06
GeorgiNzm <georginzm@gmail.com>
2016-10-24 22:06:55 +0300

    Ubuntu ne stava

---
+++ h09_nodejs_modules_tests/app_b_16/test/test.js
@@ -0,0 +1,45 @@
+var module = require('../modules/solution.js');
+
+
+	exports.test_none = function(test)
+	{
+		var input = require('./input_files/input_nan.json');
+		test.equal(({
+		    "x1": "NaN",
+		    "x2": "NaN"
+			}).to_json, module.solve(input.a, input.b, input.c));
+		    
+		test.done();
+	};
+
+	exports.test_any = function(test)
+	{
+
+		var input = require('./input_files/input_any.json');
+		test.equal(({"x": "Any"}).to_json, module.solve(input.a, input.b, input.c));
+		
+		test.done();
+	};
+
+	exports.test_D0 = function(test)
+	{
+		var input = require('./input_files/input-1.json');
+
+	    test.equal(({
+	  	"x": -1
+		}).to_json, module.solve(input.a, input.b, input.c));
+
+	test.done();
+	};
+	
+	exports.test_DGreaterThan0 = function(test)
+	{
+		var input = require('./input_files/input-2.json');
+
+	    test.equal(({
+	    "x1": -0.2,
+	    "x2": -1
+		}).to_json, module.solve(input.a, input.b, input.c));
+	    
+	    test.done();
+	};
