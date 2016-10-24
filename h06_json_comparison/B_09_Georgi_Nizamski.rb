38e05d76dbdf79cf777b2ffa646fe0e5f3564c60
GeorgiNzm <georginzm@gmail.com>
2016-10-24 08:11:25 +0300

    Updating 6th Ruby homework - JSON Comparison

--- h06_json_comparison/B_09_Georgi_Nizamski.rb
+++ h06_json_comparison/B_09_Georgi_Nizamski.rb
@@ -1,20 +1,12 @@
 require 'json'
 
-#Parsing files from JSON to Hash
-
 f_hash = JSON.parse(File.read(ARGV[0].to_s))
 s_hash = JSON.parse(File.read(ARGV[1].to_s))
 
-def is_shematic_equal (first, second)
-	#Check keys
-	is_keys_equal = (first.keys).uniq == (second.keys).uniq
-	
-	#Check values
-	is_values_equal = (first.values).uniq == (second.values).uniq
-	
-	if is_keys_equal && is_values_equal then return 1
+def is_shematic_equal?(first_json, second_json)
+	if first_json == second_json then return 1
 	else return 0
 	end
 end
 
-puts is_shematic_equal(f_hash,s_hash)
+puts is_shematic_equal?(f_hash,s_hash)
