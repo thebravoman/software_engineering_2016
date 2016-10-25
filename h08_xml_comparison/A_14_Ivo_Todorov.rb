aa0a73012bcbed33000d67feefb408fbdb747ef3
IvoTod <ivo.tod.todorov@gmail.com>
2016-10-25 21:03:11 +0300

    Fix homework 09

--- h08_xml_comparison/A_14_Ivo_Todorov.rb
+++ h08_xml_comparison/A_14_Ivo_Todorov.rb
@@ -1,7 +1,9 @@
 require "json"
-require "xmlsimple"
+require "crack"
 
-xmlData = XmlSimple.xml_in(File.read(ARGV[0]), {'KeyAttr' => 'name', "ForceArray" => false, "KeepRoot" => true})
+#Switch over to crack because closing element on new line added a key 'content' containing '\n' every time feelsbadman
+
+xmlData = Crack::XML.parse(File.read(ARGV[0]))
 jsonData = JSON.parse(File.read(ARGV[1]))
 
 puts xmlData == jsonData ? 1 : 0
