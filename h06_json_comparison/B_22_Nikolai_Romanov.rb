14fcd26b5c463039011c60ec66ad6ac7a6275709
6рек <busternr@abv.bg>
2016-10-25 11:17:17 +0300

    Fixed

--- h06_json_comparison/B_22_Nikolai_Romanov.rb
+++ h06_json_comparison/B_22_Nikolai_Romanov.rb
@@ -1,4 +1,4 @@
 require 'json'
 file1 = File.read(ARGV[0].to_s)
 file2 = File.read(ARGV[1].to_s)
-puts JSON.parse(file1) == JSON.parse(file) ? 1 : 0
+puts JSON.parse(file1) == JSON.parse(file2) ? 1 : 0
