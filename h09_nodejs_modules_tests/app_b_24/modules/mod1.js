d2cd6409c8fde985539c827b56642b6fbb62a479
Slavkata <slavkirilov00@gmail.com>
2016-10-28 20:51:15 +0300

    fixed the occuring mistakes

--- h09_nodejs_modules_tests/app_b_24/modules/mod1.js
+++ h09_nodejs_modules_tests/app_b_24/modules/mod1.js
@@ -3,35 +3,34 @@ exports.equation = function(a,b,c){
 	if (a == 0 && b != 0){
 		var x1 = +(c/b).toFixed(2);
 		final.x = x1;
-		console.log(final);
+		return final;
 	}else if (a != 0){
-	 	D = b*b - 4*a*c;
+	 	var D = b*b - 4*a*c;
 		if (D == 0){
 			var x = +(-b/2/a).toFixed(2);
 			if (x == -0){
 				var x = +Math.abs(x);
 				final.x = x;
-				console.log(final);
+				return final;
 			}
 			else{ 
 				final.x = x; 
-				console.log(final);
+				return final;
 			}
 		}else if (D > 0){
 		 	var x1 = +((-b-Math.sqrt(D))/2/a).toFixed(2);
 		 	var x2 = +((-b+Math.sqrt(D))/2/a).toFixed(2);
 		 	final.x1 = x1;
 		 	final.x2 = x2;
-			console.log(final);
+			return final;
 		}else if (D < 0){
 			final.x1 = "NaN";
 			final.x2 = "NaN";	
-			console.log(final);
+			return final;
 		}
 	}
 	if (b == 0 && a == 0 && c == 0){
 		final.x = "Any"
-		console.log(final);	
+		return final;	
 	}
-
 }
