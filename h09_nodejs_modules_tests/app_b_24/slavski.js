2b896cb2bd530e26201fda0af39ed893ec7362d3
Slavkata <slavkirilov00@gmail.com>
2016-10-28 20:17:15 +0300

    fixing the little mistakes

--- h09_nodejs_modules_tests/app_b_24/slavski.js
+++ h09_nodejs_modules_tests/app_b_24/slavski.js
@@ -1,11 +1,6 @@
 var mod = require("./modules/mod1.js");
 var fs = require ('fs');
-//files that can be used :
-//'./Tests/input_files/input-nan.json'
-//'./Tests/input_files/input-any.json'
-//'./Tests/input_files/input-1.json'
-//'./Tests/input_files/input-2.json'
-var read = fs.readFileSync('./Tests/input_files/input-nan.json');
+var read = fs.readFileSync('input-nan.json');
 var parce = JSON.parse(read);
 var ss = JSON.stringify(parce,null,2);
 var a = parce.a;
