1e8c7b202e8e0bd2897b624c374449df3ea8a2ac
Boris Mutafov <mutafow@gmail.com>
2016-10-28 22:53:22 +0300

    Update B_03_Boris_Mutafov.rb

--- h03_parse_csv/B_03_Boris_Mutafov.rb
+++ h03_parse_csv/B_03_Boris_Mutafov.rb
@@ -1,6 +1,11 @@
 require 'csv'
+
 data, output, full_data = ARGV[0], ARGV[1].to_i, Hash.new(0.0)
-CSV.foreach(data) do |row|  
-	full_data[row[ output < 3 ? 1 : 0 ]]+= output % 2 == 1 ? row[2].to_f : 1
+
+CSV.foreach(data) do |row|
+	index = output < 3 ? 1 : 0 
+	user_or_video = row[ index ]
+	percent_or_times = output % 2 == 1 ? row[2].to_f : 1
+	full_data[user_or_video] += percent_or_times
 end
 printf output % 2 == 1 ? "%i,%.2f\n" : "%i,%i\n", *full_data.max_by{|k,v| v}
