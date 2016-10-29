aa3436222cf4955edde1b8373a0ada70ee8d8462
radito3 <radito3@mail.bg>
2016-10-29 17:20:20 +0300

    Update A_25_Rangel_Ivanov.rb

--- h06_json_comparison/A_25_Rangel_Ivanov.rb
+++ h06_json_comparison/A_25_Rangel_Ivanov.rb
@@ -3,8 +3,4 @@ require'json'
 file1 = File.read(ARGV[0])
 file2 = File.read(ARGV[1])
 
-if JSON.parse(file1) == JSON.parse(file2)
-    puts 1
-else 
-    puts 0
-end
+puts JSON.parse(file1) == JSON.parse(file2) ? 1 : 0
