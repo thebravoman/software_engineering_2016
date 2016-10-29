48ed1981a92e49b556b4e963c1a59d0ea92b8aec
radito3 <radito3@mail.bg>
2016-10-29 17:15:49 +0300

    Removing redundancy

--- h02_quadratic/A_25_Rangel_Ivanov.rb
+++ h02_quadratic/A_25_Rangel_Ivanov.rb
@@ -4,6 +4,15 @@ a = ARGV[0].to_f
 b = ARGV[1].to_f
 c = ARGV[2].to_f
 
+def check (x)
+    if x % 1 == 0 
+            x = x.to_i
+        else 
+            x = x.round(2)
+        end
+    return x
+end
+
 if a == 0 && b == 0 && c == 0
     puts "#"
     exit
@@ -13,32 +22,20 @@ if a == 0
         exit
     else
     x = -c/b
-        if x % 1 == 0 
-            x = x.to_i
-        else 
-            x = x.round(2)
-        end
-    puts x.to_s
+    a1 = check(x)
+    puts a1.to_s
     exit
 end
-else if b == 0
+elseif b == 0
     x1 = Math.sqrt(-c/a)
     x2 = -Math.sqrt(-c/a)
-    if x1 % 1 == 0
-        x1 = x1.to_i
-    else
-        x1 = x1.round(2)
-    end
-    if x2 % 1 == 0
-        x2 = x2.to_i
-    else
-        x2 = x2.round(2)
-    end
-    if x1 > x2
-        puts x2.to_s + "," + x1.to_s
+    a2 = check(x1)
+    a3 = check(x2)
+    if a2 > a3
+        puts a3.to_s + "," + a2.to_s
         exit
     else
-        puts x1.to_s + "," + x2.to_s
+        puts a2.to_s + "," + a3.to_s
         exit
     end
 end
@@ -48,30 +45,18 @@ if d.real < 0
     exit
 else if d.real == 0
     x = -b/2*a
-    if x % 1 == 0
-        x = x.to_i
-    else
-        x = x.round(2)
-    end
-    puts x.to_s
+    a4 = check(x)
+    puts a4.to_s
     exit
 end
 end
 x1 = (-b+d)/2*a
 x2 = (-b-d)/2*a
-if x1 % 1 == 0
-    x1 = x1.to_i
+a5 = check(x1)
+a6 = check(x2)
+if a5.real > a6.real
+    puts a6.real.to_s + "," + a5.real.to_s
 else
-    x1 = x1.round(2)
-end
-if x2 % 1 == 0 
-    x2 = x2.to_i
-else
-    x2 = x2.round(2)
-end
-if x1.real > x2.real
-    puts x2.real.to_s + "," + x1.real.to_s
-else
-    puts x1.real.to_s + "," + x2.real.to_s
+    puts a5.real.to_s + "," + a6.real.to_s
 end
 end
