6e9f703a0b5181f2a91153d93a86855e33eae739
Boris Mutafov <mutafow@gmail.com>
2016-10-30 18:42:59 +0200

    rename to_int_if_int to to_int_if_round

--- h02_quadratic/B_03_Boris_Mutafov.rb
+++ h02_quadratic/B_03_Boris_Mutafov.rb
@@ -1,6 +1,6 @@
 a, b, c = ARGV[0].to_f, ARGV[1].to_f, ARGV[2].to_f
 
-def to_int_if_int(number)
+def to_int_if_round(number)
 	if number % 1 == 0
 		return number.to_i
 	else 
@@ -15,16 +15,16 @@ def solve_quadratic(a, b, c)
 		elsif b==0 && (c>0 || c<0) then
 			return ""
 		else
-			return to_int_if_int(-c/b)
+			return to_int_if_round(-c/b)
 		end
 	else
 		d = b*b - 4*a*c
 		if d > 0 
 			x1 =  +( (-b + Math.sqrt(d)) / (2 * a) ).round(2)
 			x2 =  +( (-b - Math.sqrt(d)) / (2 * a) ).round(2)
-			return to_int_if_int(x1) , to_int_if_int(x2)
+			return to_int_if_round(x1) , to_int_if_round(x2)
 		else
-			return to_int_if_int( (-b) / (2 * a) ).round(2)
+			return to_int_if_round( (-b) / (2 * a) ).round(2)
 		end
 	end
 end #def
