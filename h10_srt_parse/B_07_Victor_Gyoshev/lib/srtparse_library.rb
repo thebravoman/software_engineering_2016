87c42d016e42309d42d7c0470d109ed5cca84697
root <root@thecatishere.thecatishere.org>
2016-10-31 15:59:31 +0200

    end my misery

--- h10_srt_parse/B_07_Victor_Gyoshev/lib/srtparse_library.rb
+++ h10_srt_parse/B_07_Victor_Gyoshev/lib/srtparse_library.rb
@@ -11,10 +11,10 @@ module SRTParser
         sub_ID = 0
         sentence_inprogress = false
         timestring = ""
-        time_start = time_end = Array.new
+        endsub = Array.new
         File.foreach(dir) do |line|
             if line.match(/\d\d:\d\d:\d\d,\d\d\d.+/)
-                    timestring = line
+                timestring = line
             elsif line.match(/\d+/)
                 sub_ID = line.to_i
             elsif line.match(/^\n/)
@@ -46,9 +46,8 @@ module SRTParser
             end
         end
 
-        time_end = timestring.scan(/\d+/).map! { |str| str.to_i}
-        time_start = IO.readlines(dir)[1].scan(/\d+/).map! { |str| str.to_i}
-        duration = (time_end[4]-time_start[0])*3600 + (time_end[5]-time_start[1])*60 + (time_end[6]-time_start[2]) + (time_end[7]-time_start[3]).to_f/1000
+        endsub = timestring.scan(/\d+/).map! { |str| str.to_i}
+        duration = (endsub[4]*60+endsub[5])*60+endsub[6]+endsub[7].to_f/1000
         avr_symbol_line = (symbol_count/line_count.to_f).round(2)
         avr_symbol_sentence = (symbol_count/sentence_count.to_f).round(2)
         hash["number_of_words"] = word_count
@@ -58,7 +57,7 @@ module SRTParser
         hash["max_symbols_per_line"] = max_line_symbol
         hash["number_of_sentences"] = sentence_count
         hash["average_symbols_per_sentence"] = avr_symbol_sentence
-        hash["duration"] = duration.round()
+        hash["duration"] = duration.round(2)
         hash["average_duration"] = (duration/sub_ID).round(2)
         return hash
     end
