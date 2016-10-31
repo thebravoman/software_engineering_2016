9e180ad9198798cf7d8ddeb1a95e972351532af4
Slavkata <slavkirilov00@gmail.com>
2016-10-31 16:01:55 +0200

    fixed the duration method due to misunderstanding

--- h10_srt_parse/B_24_Slav_Kirilov/lib/srtparser_library.rb
+++ h10_srt_parse/B_24_Slav_Kirilov/lib/srtparser_library.rb
@@ -82,22 +82,8 @@ module SRTParser
 		end
 		def self.duration file_path
 			duration = 0.0
-			first_line_state = 0
-			last_line = 0
-			first_line = 0
 			file = File.open(file_path)
 			file.each_line do |line|
-				if line =~ /[>]/ && first_line_state == 0
-					time = line.byteslice(0,12)
-					hours = time.byteslice(0,2).to_i
-					minutes = time.byteslice(3,2).to_i
-					seconds = time.byteslice(6,2).to_i
-					miliseconds = time.byteslice(9,3).to_i
-					first_line_state = 1
-					minutes += hours*60
-					seconds += minutes*60
-					first_line = seconds.to_f + (miliseconds.to_f/1000)
-				end
 				if line =~ /[>]/
 					time = line.byteslice(15,12)
 					hours = time.byteslice(0,2).to_i
@@ -106,10 +92,9 @@ module SRTParser
 					miliseconds = time.byteslice(9,2).to_i
 					minutes += hours*60
 					seconds += minutes*60
-					last_line = seconds.to_f + (miliseconds.to_f/1000)
+					duration = seconds.to_f + (miliseconds.to_f/1000)
 				end
 			end
-			duration += last_line - first_line
 			return duration.round(2)
 		end	
 		def self.number_of_subtitles file_path
@@ -129,7 +114,7 @@ module SRTParser
 		final["max_symbols_per_line"] = max_symbols_per_line file_path
 		final["number_of_sentences"] = number_of_sentences file_path
 		final["average_symbols_per_sentence"] = ((number_of_symbols file_path).to_f / (number_of_sentences file_path).to_f).round(2)
-		final["duration"] = (duration file_path).round()
+		final["duration"] = duration file_path
 		final["average_duration"] = ((duration file_path).to_f / (number_of_subtitles file_path).to_f).round(2)
 		return final
 	end
