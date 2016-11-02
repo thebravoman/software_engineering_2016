3e643294890abd0283c99b01088a475b20a9fd86
prot3ct <djorkaevhell@abv.bg>
2016-11-02 18:53:31 +0200

    Fix HW 10

--- h10_srt_parse/A_06_Georgi_Karaboichev/lib/srtparser_library.rb
+++ h10_srt_parse/A_06_Georgi_Karaboichev/lib/srtparser_library.rb
@@ -1,75 +1,78 @@
-require "srtparser_library/version"
+#require "srtparser_library/version"
 
 module SrtparserLibrary
-    def get_words(subs)
-        return subs.scan(/[a-z'A-Z]+/);
-    end
-
-    def get_symbols(subs)
-        lines = get_lines(subs)
-        symbols_count = 0
-        
-        lines.each do |line|
-            symbols_count += line.scan(/[~!@#$%^&*()\-{}\[\]|”:><?]/).length 
+    class SRTParser
+        def get_words(subs)
+            return subs.scan(/[a-z'A-Z]+/);
         end
 
-        return symbols_count
-    end
-
-    def get_lines(subs)
-        return subs.scan(/^[ ,a-zA-Z.'!~!@#$%^&*()\-{}\[\]|”:><?]+$/)
-    end
-
-    def get_max_symbols_per_line(subs)
-        lines = get_lines(subs)
-        current = 0
-        max = 0
-
-        lines.each do |line|   
-            current = get_symbols(line)
-
-            if(max < current)
-                max = current
-            end 
-        end
-
-        return max
-    end
-
-    def get_number_of_sentances(subs)
-        return subs.scan(/[A-Z][^.?!]+[.?!]/m)
-    end
-
-    def get_all_durations(subs)
-        seconds = []
-        durations = subs.scan(/[0-9]{2}+:[0-9]{2}+:[0-9]{2}+,[0-9]{3}+/)
-
-        durations.each do |duration|
-            times_of_duration = duration.split(':')
-
-            time = 0
-            multiplier = 3600
-            duration_in_seconds = 0.0
-            times_of_duration.each do |time|
-                time = time.gsub(',', '.')
-                duration_in_seconds += time.to_f * multiplier.to_f  
-                multiplier /= 60.0
+        def get_symbols(subs)
+            lines = get_lines(subs)
+            symbols_count = 0
+            
+            lines.each do |line|
+                symbols_count += line.scan(/[~!@#$%^&*()\-{}\[\]|”:><?]/).length 
             end
 
-            duration_in_seconds = duration_in_seconds.round()
-            seconds.push(duration_in_seconds)
+            return symbols_count
         end
 
-        return seconds
-    end
+        def get_lines(subs)
+            return subs.scan(/^[ ,a-zA-Z.'!~!@#$%^&*()\-{}\[\]|”:><?]+$/)
+        end
 
+        def get_max_symbols_per_line(subs)
+            lines = get_lines(subs)
+            current = 0
+            max = 0
 
-    class SRTParser
-        def parse_file()
-            subs = File.read("test.srt")
+            lines.each do |line|   
+                current = get_symbols(line)
+
+                if(max < current)
+                    max = current
+                end 
+            end
+
+            return max
+        end
+
+        def get_number_of_sentances(subs)
+            return subs.scan(/[A-Z][^.?!]+[.?!]/m)
+        end
+        
+        def get_all_durations(subs)
+            seconds = []
+            durations = subs.scan(/[0-9]{2}+:[0-9]{2}+:[0-9]{2}+,[0-9]{3}+/)
+
+            durations.each do |duration|
+                times_of_duration = duration.split(':')
+
+                time = 0
+                multiplier = 3600
+                duration_in_seconds = 0.0
+                times_of_duration.each do |time|
+                    time = time.gsub(',', '.')
+                    duration_in_seconds += time.to_f * multiplier.to_f  
+                    multiplier /= 60.0
+                end
+
+                duration_in_seconds = duration_in_seconds.round()
+                seconds.push(duration_in_seconds)
+            end
+
+            return seconds
+        end
+        
+        def subtitles_count(subs)
+            return subs.scan(/^[0-9]+$/)
+        end
+
+        def parse_file(path)
+            subs = File.read(path)
 
             durations = get_all_durations(subs)
-            number_of_subtitle = subs.scan(/^[0-9]+$/)
+            number_of_subtitle = subtitles_count(subs) 
 
             result = Hash.new
             result["number_of_words"] = get_words(subs).length
@@ -82,11 +85,7 @@ module SrtparserLibrary
             result["duration"] = durations[durations.length - 1] # - duration[0]
             result["average_duration"] = (result["duration"].to_f / number_of_subtitle[number_of_subtitle.length - 1].to_f).round(2)
 
-            puts result["average_duration"]
+            return result
         end 
     end
-
-    srt_parser = SRTParser. new
-    srt_parser.parse_file()
-
 end
