f963a33a8d5c5593066bb41aefa24b5ece3f9156
IvoTod <ivo.tod.todorov@gmail.com>
2016-11-02 21:27:52 +0200

    Fix some stuff

--- h10_srt_parse/A_14_Ivo_Todorov/lib/srtparser_library.rb
+++ h10_srt_parse/A_14_Ivo_Todorov/lib/srtparser_library.rb
@@ -3,7 +3,7 @@ require "./srtparser_library/version.rb"
 module SRTParser
 	def self.parse_file(path_to_file)
 		file = File.open(path_to_file)
-		data = File.read(file).scan(/(\d+)\n(\d\d:\d\d:\d\d,\d\d\d) --> (\d\d:\d\d:\d\d,\d\d\d)\n((?:.+\n?)+)/) 
+		data = File.read(file).scan(/(\d+)\s*\n(\d\d:\d\d:\d\d,\d\d\d) --> (\d\d:\d\d:\d\d,\d\d\d)\n((?:.+\n?)+)/) 
 		file.close
 		number_of_words = number_of_symbols = number_of_lines = max_symbols_in_line = number_of_sentences = 0
 		current_sentence = ""
@@ -46,7 +46,7 @@ module SRTParser
 	end
 	private_class_method
 	def self.parse_time(time)
-		times = time.scan(/(?:[1-9]+[0-9]*,[0-9]+)|(?:[1-9]+[0-9]*)/)
+		times = time.scan(/(?:[1-9]*[0-9],[0-9]+)|(?:[1-9][0-9]*)/)
 		modifier = times.size
 		out = 0
 		times.each do |time|
