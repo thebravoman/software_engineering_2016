01ad02876fd342f891b9dcb258d5f242543c3e1a
Willuminat <velislavvelikov2@gmail.com>
2016-11-07 21:47:29 +0200

    oops

--- h13_query_to_json/app_b_06/B_06_Velislav_Velikov.js
+++ h13_query_to_json/app_b_06/B_06_Velislav_Velikov.js
@@ -1,10 +1,10 @@
-var protocol_ = require('http');
-var locator_ = require('url');
-var cntnt_type = require('content-type');
+var http = require('http');
+var url = require('url');
+var contnt_type = require('content-type');
 
 function protocol_req(req, resp)
 {
-	cntnt_type.format({type : "application/json"});
+	contnt_type.format({type : "application/json"});
 	var data_ = url.parse(req.url, true);
 	resp.end(JSON.stringify(data_.query, null, 2));	
 }
