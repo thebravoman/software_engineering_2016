31b6d2f8bf38227d8e4f731caa8d0ffa3994c77f
Borislav Borisov <borislav_sofroniev1975@abv.bg>
2016-11-07 13:47:03 -0800

    Update index.js

--- h13_query_to_json/app_b_04/index.js
+++ h13_query_to_json/app_b_04/index.js
@@ -1,1 +1,16 @@
 
+var http = require('http');
+var url = require('url');
+var content_t = require('content-type');
+
+
+function handle_req(request, response)
+{
+
+    content_t.format({type : "application/json"});
+    var get_data = url.parse(request.url, true);
+    response.end(JSON.stringify(get_data.query, null, 2));
+
+}
+
+http.createServer(handle_req).listen(8212, '127.0.0.1');
