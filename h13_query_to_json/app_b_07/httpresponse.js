8feb03f9a59e054ea361179748080a783557c138
Catishere <viktor.gyoshev@gmail.com>
2016-11-09 13:20:23 +0200

    Update httpresponse.js

--- h13_query_to_json/app_b_07/httpresponse.js
+++ h13_query_to_json/app_b_07/httpresponse.js
@@ -4,7 +4,7 @@ var server = http.createServer(handleRequest);
 function handleRequest(request, response){
     response.end(response.writeHead(200, {
                             'Content-Type' : 'application/json'            
-                    }); 
+                    })); 
     response.end(JSON.stringify((url.parse(request.url, true)).query,null,2));
 }
 server.listen(8207);
