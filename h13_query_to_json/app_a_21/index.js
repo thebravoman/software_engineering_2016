301fc4a1097e327106673a2007472fc4dc6f2d22
Martin Bozhinov <jkata@mail.bg>
2016-11-14 13:29:19 -0500

    Add files via upload

---
+++ h13_query_to_json/app_a_21/index.js
@@ -0,0 +1,18 @@
+
+var http = require('http');
+var url = require('url');
+var ContentType = require('content-type');
+
+function handle_request(request, response)
+{
+
+	contentType.format({type : "application/json"});
+	var get_params = url.parse(request.url, true);	
+	response.end(JSON.stringify(get_params.query,null,1));
+}
+
+
+console.log('listening on localhost:8121');
+
+
+http.createServer(handleRequest).listen(8121, '127.0.0.1');
