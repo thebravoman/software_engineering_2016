ad87a7c69b50055e0f03a5a526c01a3d743bfc15
Zachary Georgiev <zax333@abv.bg>
2016-11-15 15:23:10 +0200

    Quick fix

--- h13_query_to_json/app_a_09/query_parse.js
+++ h13_query_to_json/app_a_09/query_parse.js
@@ -1,11 +1,12 @@
 var http = require('http');
 var url = require('url');
-var contentType = require('content-type');
 var port = 8109;
 
 http.createServer(function(request, response)
 {
-	contentType.format({type: "application/json"});
 	var queryObject = url.parse(request.url, true).query;
+	response.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
 	response.end(JSON.stringify(queryObject, null, 2));
 }).listen(port, '127.0.0.1');
