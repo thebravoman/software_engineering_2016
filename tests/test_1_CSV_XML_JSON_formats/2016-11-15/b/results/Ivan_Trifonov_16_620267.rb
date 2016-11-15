287c3eafb553bf7bd81b335dbd2eeb327820ca79
Ivan3fonov <ivan3fonov@gmail.com>
2016-11-15 12:36:41 +0200

    Update Ivan_Trifonov_16_620267.rb

--- tests/test_1_CSV_XML_JSON_formats/2016-11-15/b/results/Ivan_Trifonov_16_620267.rb
+++ tests/test_1_CSV_XML_JSON_formats/2016-11-15/b/results/Ivan_Trifonov_16_620267.rb
@@ -8,8 +8,10 @@ count = 0
 number_of_values =0
 data1=JSON.parse(File.read(json))
 CSV.foreach(csv) do |row|
-		if(	data1[count] != data[row[1])
+		if(	data1[count] != data[row[1]])
 			number_of_values += 1;
 		end
 		count +=1;
-end			
+end	
+
+puts  number_of_values		
