fec7113db009d97a55d95ef69606de79bf505246
Martin Bozhinov <jkata@mail.bg>
2016-11-15 06:22:58 -0500

    Add files via upload

--- h13_query_to_json/app_a_21/index.js
+++ h13_query_to_json/app_a_21/index.js
@@ -1,25 +1,18 @@
+var http = require('http');
+var url = require('url');
 
-const http = require('http');
-const url = require('url');
-const port = 8121;
 
-function handle_request(request, response) {
-  switch (request.method) {
-    case 'GET':
-      let query = url.parse(request.url, true).query;
-      response.writeHead(200, {
-        'Content-Type': 'application/json'
-      });
-      response.end(JSON.stringify(query, null, 4));
-      break;
-    default:
-      response.end();
-  }
+function handleRequest(request, response)
+{
+
+	contentType.format({type : "application/json"});
+	  response.writeHead(200, {
+			'Content-Type' : 'application/json'    	
+	  });
+	response.end(JSON.stringify(get_params.query,null,2));
 }
 
-http.createServer(handle_request).listen(port, '127.0.0.1', function (err) {
-  if (err) {
-    throw err;
-  }
-  console.log(`Listening on port ${port}`);
-});_request).listen(8121, '127.0.0.1');
+
+
+
+http.createServer(handleRequest).listen(8121, '127.0.0.1');
