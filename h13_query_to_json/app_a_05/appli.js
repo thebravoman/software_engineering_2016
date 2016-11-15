ed5d1bfbb7a56ac757084cb9e33f285ee11ba5e3
VelislavYotov <slaviy999@abv.bg>
2016-11-15 13:15:46 +0200

    Update appli.js

--- h13_query_to_json/app_a_05/appli.js
+++ h13_query_to_json/app_a_05/appli.js
@@ -1,17 +1,13 @@
-var http=require('http');
+const http = require('http');
+const url = require('url');
 
-var url=require('url');
-
-var content_type = require('content-type');
-
-function handleRequest(request,response)
-{
-	var data=url.parse(request.url, true);
-	content_type.format({type:"application/json"});
-	response.end(JSON.stringify(data.query));
+function handleRequest(request, response) {
+let query = url.parse(request.url, true).query;
+response.writeHead(200, {
+'Content-Type': 'application/json'
+});
+response.end(JSON.stringify(query, null, 4));
 }
 
-
-console.log('listening on localhost:8105');
-
-http.createServer(handleRequest).listen(8105,'127.0.0.1');
+http.createServer(handleRequest).listen(8105, '127.0.0.1');
+console.log('Listening on server.... ');
