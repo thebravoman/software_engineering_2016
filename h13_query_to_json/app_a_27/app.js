2b14be62c76f17581d7f6b42b32badb232efc2e7
Stoyan-Ivanov <stoyaniviv@gmail.com>
2016-11-15 13:43:45 +0200

    Add files via upload

--- h13_query_to_json/app_a_27/app.js
+++ h13_query_to_json/app_a_27/app.js
@@ -4,13 +4,13 @@ var port = 8127;
 
 function Handle_GET_Request(request, response)
 {
+	var content = URL.parse(request.URL, true);
+	
 	response.writeHead(200, {
         'Content-Type': 'application/json'
 	});
-	
-	var content = URL.parse(request.URL, true);
 
 	response.end(JSON.stringify(content.query));
 }
 
-HTTP.createServer(Handle_GET_Request).listen(port, '127.0.0.1');
+HTTP.createServer(Handle_GET_Request).listen(8127, '127.0.0.1');
