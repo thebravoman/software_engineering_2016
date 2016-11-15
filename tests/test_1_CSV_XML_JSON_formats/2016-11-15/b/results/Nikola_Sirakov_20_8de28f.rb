b8879c5e294d14e4d13b4ccc97104bd78c19f66f
bubba99 <bumbalbee@abv.bg>
2016-11-15 12:40:07 +0200

    Add files via upload

---
+++ tests/test_1_CSV_XML_JSON_formats/2016-11-15/b/results/Nikola_Sirakov_20_8de28f.rb
@@ -0,0 +1,14 @@
+require 'csv'
+
+def method(one: 1, two: 2, **)
+
+header C1, header C2, header C3, header C4
+summin 1, summin 2, summin 3, new value 1
+summin 4, summin 5, summin 6, new value 2
+
+data, output, full_data = ARGV[0], ARGV[0].to_i, Hash.new(0.0)
+
+if (C1<C4) || (C3==C4+C3)
+<p>C3: <%=h @user.full_name %></p>
+ end
+end
