f11b072583158d8125a384d842e685c2a421dcfa
alvelinov <alvelinov7@gmail.com>
2016-11-15 13:14:46 +0200

    Fix hw13

--- h13_query_to_json/app_a_29/index.js
+++ h13_query_to_json/app_a_29/index.js
@@ -1,14 +1,13 @@
 var http = require('http');
 var url = require('url');
-var contentType = require('content-type');
 
 function processGetRequest(request, response) {
     console.log('Getting')
-    contentType.format({
-        type: "application/json"
-    });
     var get_params = url.parse(request.url, true);
     var object = JSON.stringify(get_params.query);
+    response.writeHead(200, {
+    	'Content-Type': 'application/json'
+    });
     response.end(object);
 }
 http.createServer(processGetRequest).listen(8129, '127.0.0.1', function(){
