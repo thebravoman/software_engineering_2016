bd15c59881a49fc5429eb15e3abe5e32c849a457
PetkoMihaylov <pet99@mail.bg>
2016-11-16 16:34:45 +0200

    Update index.js

--- h13_query_to_json/app_a_24/index.js
+++ h13_query_to_json/app_a_24/index.js
@@ -2,15 +2,13 @@ var http = require('http');
 var url = require('url');
 var data;
 
-var content_type = require('content_type');
-
-function handleRequest(request, response)
+function handleGetRequest(request, response)
 {
-	content_type.format( {type : "application/json"} );
+	response.writeHead(200, {'Content-Type' : "application/json"});
 	data = url.parse(request.url, true);	
 	response.end(JSON.stringify(data.query));
 }
 
 console.log('listening on localhost:8124');
 
-http.createServer(handleRequest).listen(8124, '127.0.0.1');
+http.createServer(handleGetRequest).listen(8124, '127.0.0.1');
