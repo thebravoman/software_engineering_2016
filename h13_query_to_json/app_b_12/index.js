f078f6f5de9b742854a5bec15c7a1a28108c1e0f
alvelinov <alvelinov7@gmail.com>
2016-11-16 13:13:26 +0200

    Fixing homework

--- h13_query_to_json/app_b_12/index.js
+++ h13_query_to_json/app_b_12/index.js
@@ -1,12 +1,11 @@
 var http = require('http');
 var url = require('url');
-var content_t = require('content-type');
 
 
 function handle_req(request, response)
 {
 
-    content_t.format({type : "application/json"});
+    response.setHeader("Content-Type", "application/json");
     var get_data = url.parse(request.url, true);
     response.end(JSON.stringify(get_data.query, null, 2));
 
