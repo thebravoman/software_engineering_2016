73b78368c49498ce12793f28295db852fcf60cd8
Cepe6 <vmladenov22@gmail.com>
2016-11-17 20:55:42 +0200

    Update homework - fix contentType

--- h13_query_to_json/app_b_08/index.js
+++ h13_query_to_json/app_b_08/index.js
@@ -1,9 +1,10 @@
 var http = require('http');
 var url = require('url');
-var contentType = require('content-type');
 
 http.createServer(function requestHandler(request, response) {
-    contentType.format({type : "application/json"});
+    response.writeHead (200, {
+        "contentType": "application/json"
+    });
 	var requestData = url.parse(request.url, true);
     response.end(JSON.stringify(requestData.query,null , 2));
 }).listen(8208, '127.0.0.1');
