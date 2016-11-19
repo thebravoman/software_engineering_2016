4339c066c3ac1ba45cfc47df8fecb93916cab0e9
Slavkata <slavkirilov00@gmail.com>
2016-11-19 13:11:01 +0200

    fixed header

--- h13_query_to_json/app_b_24.js
+++ h13_query_to_json/app_b_24.js
@@ -4,7 +4,9 @@ var contentType = require('content-type');
 
 function handleRequest(request, response)
 {
-	contentType.format({type : "application/json"});
+    response.writeHead(200, {
+        'Content-Type' : 'application/json'        
+    });
     var get_params = url.parse(request.url, true);
     var get_query = get_params.query;
     response.end(JSON.stringify(get_query, null, 2));
