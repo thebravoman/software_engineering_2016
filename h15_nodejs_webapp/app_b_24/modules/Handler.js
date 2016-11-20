70a48440b9658ed4eac937c5a3b9dc2b070f6ea4
Slavkata <slavkirilov00@gmail.com>
2016-11-20 10:26:57 +0200

    Update Handler.js

--- h15_nodejs_webapp/app_b_24/modules/Handler.js
+++ h15_nodejs_webapp/app_b_24/modules/Handler.js
@@ -1,12 +1,12 @@
 var url = require('url');
-var responseWriter = require('./requestWriter.js');
+var Writer = require('./Writer.js');
 
 exports.Handler = function(request, response) {
     var queueParams = url.parse(request.url, true);
     if(queueParams.query.image != null) {
-        responseWriter.writeResponse('image/image.jpeg', {'contentType': 'image/jpeg'}, response);
+        Writer.writeResponse('image/image.jpeg', {'contentType': 'image/jpeg'}, response);
     } else {
-        responseWriter.writeResponse('data/data.json', {
+        Writer.writeResponse('data/data.json', {
             'contentType': 'application/json',
             'Image-Url':'http://localhost:8224/?image'
             }, response);
