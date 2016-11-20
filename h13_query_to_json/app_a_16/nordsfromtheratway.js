4c9b413106cc66c9087a6ad23f69677d71fd5f63
iordanivanov <iiordan.ivanov@gmail.com>
2016-11-20 17:44:55 +0200

    Update nordsfromtheratway.js
    late fix please giv good tochki sorry for being late ;/

--- h13_query_to_json/app_a_16/nordsfromtheratway.js
+++ h13_query_to_json/app_a_16/nordsfromtheratway.js
@@ -1,11 +1,12 @@
-var http = require('http');
-var url = require('url');
-var content_type = require("content-type");
+const http = require('http');
+const url = require('url');
 
 function handleRequest(request, response) {
-    var urldata = url.parse(request.url, true);
-    content_type.format({type: "application/json"});
-    response.end(JSON.stringify(urldata.query));
+    var urldata = url.parse(request.url, true).query;
+    response.writeHead(200, {
+    'Content-Type': 'application/json'
+    });
+    response.end(JSON.stringify(urldata, null, 4));
 }
 
 http.createServer(handleRequest).listen(8116, '127.0.0.1');
