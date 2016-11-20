3498c280aa58f02065b9c60fb83bd3b2c40643f4
GeorgiNzm <georginzm@gmail.com>
2016-11-20 08:31:52 +0200

    Adding 15th NodeJS homework - GET processing with image

---
+++ h15_nodejs_webapp/app_b_09/modules/handler.js
@@ -0,0 +1,40 @@
+var url = require('url');
+var fs = require('fs');
+
+function provideData(filename, contentType, response) {
+	
+	fs.access(filename, function(error, access) {
+		if (error) {
+			response.writeHead(404);
+			response.end("Not found");
+		} else {
+			fs.readFile(filename, function (error, data) {
+				if (error) {
+					response.writeHead(500);
+					response.end("Internal server error");
+				} else {
+					response.writeHead(200, contentType);
+					response.end(data);
+				}
+			});
+		}
+	});
+}
+
+exports.proccessGETRequest = function(request, response) {
+	
+	var get_parametres = url.parse(request.url, true);
+	if (get_parametres.query.data != null) {
+		provideData('data/data.json', 
+								 {'Content-Type': 'application/json'},
+								 response);
+	} else if (get_parametres.query.image != null) {
+		provideData('image/image.jpg',
+								 {'Content-Type': 'image/jpeg'},
+								 response);
+	} else {
+		provideData('data/data.json', 
+				 {'Content-Type': 'application/json'},
+				 response);
+	}
+}
