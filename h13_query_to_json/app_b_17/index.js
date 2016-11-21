0ce8b042e8f2e04747cde43d86113276ee5d5e41
lmladenov <lachomladenov@gmail.com>
2016-11-21 17:24:21 +0200

    Update index.js

--- h13_query_to_json/app_b_17/index.js
+++ h13_query_to_json/app_b_17/index.js
@@ -1,12 +1,14 @@
 var http = require('http');
 var url = require('url');
-var contentType = require('content-type');
 
-function handleRequest(request, response) {
-	
-	contentType.format({type : "application/json"});
-	var requestData = url.parse(request.url, true);
-	response.end(JSON.stringify(requestData.query,null , 2));
+function handleRequest(request, response){
+	 response.writeHead(200, {
+		 'Content-Type' : 'application/json'}
+     );
+	 
+	 var data = url.parse(request.url, true);
+	 
+     response.end(JSON.stringify(data.query, null, 2));
 }
 
-http.createServer(handleRequest).listen(8217, '127.0.0.1'); 
+http.createServer(handleRequest).listen(8217, '127.0.0.1');
