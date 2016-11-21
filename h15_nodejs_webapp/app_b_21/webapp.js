8aee453d23172a69162cdf5c9ad92ec380aedac7
nikolatzotchev <ntzotchev@gmail.com>
2016-11-21 21:14:38 +0200

    still using this to push never mind program is ready

---
+++ h15_nodejs_webapp/app_b_21/webapp.js
@@ -0,0 +1,23 @@
+/**
+ * http://usejsdoc.org/
+ */
+var http = require('http');
+var url = require('url');
+var dataProvider = require('./modules/data.js');
+var port = 8221;
+
+function handlingRequest(request, response)
+{
+	var get_params = url.parse(request.url, true);	
+	if (get_params.query.image != null)
+	{
+		dataProvider.provideData('./images/image.jpg' , {'Content-Type': 'image/jpeg'} , response);
+	}
+	else {
+			dataProvider.provideData('./data/data.json', {
+					'Content-Type': 'application/json',
+					'Image-Url': 'http://localhost:8208/?image'
+			}, response);
+		}	
+}	
+http.createServer(handlingRequest).listen(port, 'localhost');
