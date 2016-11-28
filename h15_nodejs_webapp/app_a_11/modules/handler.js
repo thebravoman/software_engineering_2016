7dc03ff001c2abe062203b5c4aaebefdea79a0ab
ivanmilevtues <ivanmilevtues@gmail.com>
2016-11-28 15:35:31 +0200

    Some work on NodeJS homework and Ruby password generator started writing it in text file.

--- h15_nodejs_webapp/app_a_11/modules/handler.js
+++ h15_nodejs_webapp/app_a_11/modules/handler.js
@@ -20,5 +20,50 @@ function openData(filename, headerInfo, response) {
 }
 
 exports.provideData = function(filename, headerInfo, response) {
-    openData(filename, headerInfo, response)
-}
+    openData(filename, headerInfo, response);
+};
+
+exports.queryData = function(filename, headers, key, query, response) {
+    console.log("Querry Data called...");
+	fs.exists(filename, function(exists) {
+		if (exists) {
+            console.log('File found');
+		    fs.readFile(filename, function(error, data) {
+		        if (!error) {
+					var result = {};
+					var filteredData = [];
+					var allData = JSON.parse(data);
+                    console.log("Reading file...");
+					if (Array.isArray(allData.characters)) {
+                        console.log("This is array");
+                        allData.characters.forEach(function(character) {
+                            console.log(character[key] + ' ' + query);
+							if (character[key] === query) {
+								filteredData.push(character);
+							}
+						});
+					}
+					if (filteredData.length > 0) {
+						result[query] = filteredData;
+						var imageUrl = 'images/' + query;
+						headers["Image-Url"] = 'http://localhost:8180/?image='+query;
+					}
+
+
+					response.writeHead(200, headers);
+                    console.log(result);
+					response.end(JSON.stringify(result));
+				}
+				else {
+					response.writeHead(500);
+					response.end('Internal Server Error');
+				}
+			});
+		}
+		else
+		{
+			response.writeHead(404);
+			response.end('Image not found');
+		}
+	});
+};
