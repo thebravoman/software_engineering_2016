868b889e92e1a726cf7be1abf33ce800d5ddc682
thecatishere <thecatishere@thecatishere.thecatishere.org>
2016-11-28 23:02:23 +0200

    fix

--- h16_nodejs_query_filtering/app_b_07/app.js
+++ h16_nodejs_query_filtering/app_b_07/app.js
@@ -7,25 +7,25 @@ var hostname = 'localhost';
 
 function handleRequest(request, response)
 {
-	if (request.url === '/favicon.ico')
-	{
-		console.log('Ignore favicon request...');
-	}
-	else
-	{	
-		var get_params = url.parse(request.url, true);
-		if (get_params.query.image)
-		{
-			dataProvider.provideData('images/' + get_params.query.image + '.jpg', {'Content-Type': 'image/jpeg'}, response);
-		}
-		else if (Object.keys(get_params.query).length)
-		{
-			dataProvider.queryData('data/data.json',{'Content-Type': 'application/json'}, get_params.query, response);
-		}
-		else
-		{
-			dataProvider.provideData('data/data.json',{'Content-Type': 'application/json'}, response);
-		}
-	}
+    if (request.url === '/favicon.ico')
+    {
+        console.log('Ignore favicon request...');
+    }
+    else
+    {
+        var get_params = url.parse(request.url, true);
+        if (get_params.query.image)
+        {
+            dataProvider.provideData('images/' + get_params.query.image + '.jpg', {'Content-Type': 'image/jpeg'}, response);
+        }
+        else if (Object.keys(get_params.query).length)
+        {
+            dataProvider.queryData('data/data.json',{'Content-Type': 'application/json'}, get_params.query, response);
+        }
+        else
+        {
+            dataProvider.provideData('data/data.json',{'Content-Type': 'application/json'}, response);
+        }
+    }
 }
 http.createServer(handleRequest).listen(port, hostname);
