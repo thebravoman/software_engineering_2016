0cf850f2af225998970810dd56c73fee27909910
ivaylo99 <ivaylo_vasilev99@abv.bg>
2016-11-28 23:55:50 +0200

    Update app.js

--- h16_nodejs_query_filtering/app_b_14/app.js
+++ h16_nodejs_query_filtering/app_b_14/app.js
@@ -1,16 +1,33 @@
 var http = require('http');
 var url = require('url');
-var dataProvider = require('./modules/data_provider.js');
-function requestHandler(request, response) {
-	var get_params = url.parse(request.url, true);
-		if (get_params.query.image != null && get_params.query.image != null)
+var dataProvider = require('./modules/data-provider.js');
+
+var port = 8224;
+var hostname = 'localhost';
+
+function handleRequest(request, response)
+{
+	if (request.url ==='/favicon.ico')
+	{
+		console.log('Ignore facicon request...');
+	}
+	else
+	{	
+		var get_params = url.parse(request.url, true);
+		if (get_params.query.image)
 		{
-			dataProvider.provideData('image/image.jpg',{'Content-Type': 'image/jpeg'}, response);
+			dataProvider.provideData('images/'+get_params.query.image+'.jpg',{'Content-Type': 'image/jpeg'}, response);
+		}
+		else if (Object.keys(get_params.query).length)
+		{
+			console.log("init");
+			dataProvider.queryData('data/data.json',{'Content-Type': 'application/json'}, get_params.query, response);
 		}
 		else
 		{
-			dataProvider.provideData('data/data.json',{'Content-Type': 'application/json', 'Image-Url': 'http://localhost:8215/?image'}, response);
+			dataProvider.provideList('data/data.json',{'Content-Type': 'application/json'}, response);
 		}
+	}
 }
 
-http.createServer(requestHandler).listen(8215, 'localhost');
+http.createServer(handleRequest).listen(port, hostname);
