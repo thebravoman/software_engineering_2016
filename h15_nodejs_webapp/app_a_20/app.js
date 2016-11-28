24fa6a33f3606772a25c6ff20d447d62e6acfcbf
MarchiT <mrtihova@gmail.com>
2016-11-28 23:47:11 +0200

    Code refactoring

--- h15_nodejs_webapp/app_a_20/app.js
+++ h15_nodejs_webapp/app_a_20/app.js
@@ -12,7 +12,6 @@ function handleRequest(request, response) {
 	var query = url.parse(request.url, true).query;
 
 	if(query.image != null){
-		console.log(query.image);
 		handler.provideData('./images/'+ query.image +'.jpg',{'Content-Type': 'image/jpeg'}, response);
 	} else if (query != null && Object.keys(query).length > 0) {
 		handler.queryData('./data/data.json', {'Content-Type': 'application/json'}, query, response);
