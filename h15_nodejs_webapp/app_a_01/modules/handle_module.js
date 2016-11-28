7ff44bc624c7899f1ec271c6546b778896321241
Yellobelly <alexstoyanof@gmail.com>
2016-11-28 22:18:59 +0200

    working 1v4n40

--- h15_nodejs_webapp/app_a_01/modules/handle_module.js
+++ h15_nodejs_webapp/app_a_01/modules/handle_module.js
@@ -3,66 +3,74 @@ var fs = require('fs');
 exports.provideData=function readData(filename, contentType, response)
 {
 
-	fs.exists(filename, function(exists) {
-        if (exists) {
-            fs.readFile(filename, function(error, data) {
-                if (!error) {
+					fs.exists(filename, function(exists) {
+						if (exists) {
+							fs.readFile(filename, function(error, data) {
+						if (!error) {
 
-			              response.writeHead(200, contentType);
-                    response.end(data);
-                } else {
-                     response.writeHead(500);
-                    response.end('Internal Server Error');
-                }
-            });
-        } else {
-            response.writeHead(404);
-            response.end('File not found');
-        }
-    });
+							response.writeHead(200, contentType);
+							response.end(data);
+						} else {
+							response.writeHead(500);
+							response.end('Internal Server Error');
+						}
+						});
+						} else {
+							response.writeHead(404);
+							response.end('File not found');
+						}
+					});
 
 };
 
-exports.queryData=function readData(filename, headers, query, response)
-{
-
+exports.queryData = function(filename, headers, query, response) {
 	fs.exists(filename, function(exists) {
-        if (exists) {
-            fs.readFile(filename, function(error, data) {
-                if (!error) {
-										var result = {};
-										var data_after_querry = [];
-										var data_ =  JSON.parse(data);
-										var arrayLength = data_.length;
-
-										for (var i = 0; i < arrayLength; ++i) {
-												var character = data_[i];
-												if (character.type === query) {
-													data_after_querry.push(character);
+		if (exists) {
+			fs.readFile(filename, function(error, data) {
+						if (!error)	{
+							var result = {};
+							var data_after_querry = [];
+							var data_ = JSON.parse(data);
+		                    var already_done;
+							if (Array.isArray(data_.characters)){
+								data_.characters.forEach(function(character) {
+								for(var key in query) {
+								if(key in character) {
+										already_done = false;
+										if (character[key] == query[key]) {
+											for(var i = 0; i < data_after_querry.length; i++) {
+												if(data_after_querry[i] == character) {
+												already_done = true;
+												}
+											}
+											if(!already_done) {
+											data_after_querry.push(character);
 											}
 										}
-
-
-										if (data_after_querry.length > 0) {
-											result[query] = filteredData;
-											var imageUrl = 'images/' + query;
-											headers["Image-Url"] = 'http://localhost:8101/?image='+query;
 										}
+								}
+						});
+						}
+						if (data_after_querry.length > 0) {
+							result = data_after_querry;
+							headers["Image-Url"] = 'http://localhost:8111/?image='+data_after_querry.type;
+						}
 
 
-										response.writeHead(200, headers);
-										response.end(JSON.stringify(result));
+						response.writeHead(200, headers);
 
-
-                } else {
-                     response.writeHead(500);
-                    response.end('Internal Server Error');
-                }
-            });
-        } else {
-            response.writeHead(404);
-            response.end('File not found hahaha');
-        }
-    });
-
-};
+						response.end(JSON.stringify(result));
+						}
+						else {
+							response.writeHead(500);
+							response.end('Internal Server Error');
+						}
+						});
+						}
+						else
+						{
+							response.writeHead(404);
+							response.end('Image not found');
+						}
+						});
+						};
