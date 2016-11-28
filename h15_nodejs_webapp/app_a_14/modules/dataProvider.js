2eb77e6546d88077f203024b23f9471d8e19beea
IvoTod <ivo.tod.todorov@gmail.com>
2016-11-28 19:53:40 +0200

    Update app for homework and push version to 1.0.2

--- h15_nodejs_webapp/app_a_14/modules/dataProvider.js
+++ h15_nodejs_webapp/app_a_14/modules/dataProvider.js
@@ -1,28 +1,95 @@
-const fs = require("fs");
+var fs = require('fs');
 
-function getData(filePath, headers ,response) {
-    fs.exists(filePath, function(exists) {
-        if(exists){
-            fs.readFile(filePath, function(err, data) {
-                if(err){
-                    console.log(err);
-                    response.writeHead(500);
-                    response.end("Internal server error!");
-                }
-                else {
-                    console.log(typeof data);
-                    response.writeHead(200, headers);
-                    response.end(data);
-                }
-            });
+function readData(filename, contentType, response)
+{
+    console.log('providing ' + filename);
+    fs.exists(filename, function(exists) {
+        if (exists) {
+                fs.readFile(filename, function(error, data) {
+                    if (!error) {
+                        // console.log(data);
+                        response.writeHead(200, contentType);
+                        response.end(data);
+                    }
+                    else {
+                        response.writeHead(500);
+                        response.end('Internal Server Error');
+                    }
+                });
         }
-        else{
+        else
+        {
             response.writeHead(404);
-            response.end("Image not found!");
+            response.end('Image not found');
         }
-    })
+    });
 }
 
-exports.provideData = function (filePath, headers, response){
-    return getData(filePath, headers, response);
-}
+
+
+exports.provideData = function(filename, contentType, response)
+{
+    readData(filename,contentType, response);
+};
+
+exports.provideList = function(filename, contentType,  response)
+{
+    // console.log("Providing list");
+    readData(filename,contentType, response);
+};
+
+exports.queryData = function(filename, headers, query, response) {
+    fs.exists(filename, function(exists) {
+        if (exists) {
+                fs.readFile(filename, function(error, data) {
+                    if (!error) {
+                        var filteredData = [];
+                        var allData = JSON.parse(data);
+
+                        if (Array.isArray(allData.characters)){
+
+                            allData.characters.forEach(function(character) {
+                                let same = false;
+                                // console.log("Going through character " + character.type);
+                                for(let key in query) {
+                                    if(key in character) {
+                                        // console.log("Key: " + key + " | query key: " + query[key] + " | character key: " + character[key]);
+                                        if(character[key] == query[key]){
+                                            same = true;
+                                        }
+                                    }
+                                }
+                                if(same){
+                                    filteredData.push(character);
+                                }
+
+                            });
+                        }
+                        //Write in Image-Url the url to the first found character in case its needed for homework again
+                        if (filteredData.length > 0) {
+                            if(filteredData[0].type == undefined){
+                                headers["Image-Url"] = "Cannot extract image URL from character type";
+                            }
+                            else{
+                                headers["Image-Url"] = 'http://localhost:8180/?image='+filteredData[0].type;
+                            }
+                        }
+
+                        //console.log(filteredData);
+
+                        response.writeHead(200, headers);
+                        response.end(JSON.stringify(filteredData));
+                    }
+                    else {
+                        response.writeHead(500);
+                        response.end('Internal Server Error');
+                    }
+                });
+        }
+        else
+        {
+            response.writeHead(404);
+            response.end('Image not found');
+        }
+    });
+};
