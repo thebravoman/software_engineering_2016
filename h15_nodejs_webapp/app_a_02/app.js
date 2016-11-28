ac076ccffcb51eb60d71a8218692f81cbacdd03b
vincho7012 <mihailovblagovest@gmail.com>
2016-11-28 18:04:18 +0200

    new version

--- h15_nodejs_webapp/app_a_02/app.js
+++ h15_nodejs_webapp/app_a_02/app.js
@@ -1,3 +1,4 @@
+
 var http = require('http');
 var url = require('url');
 var dataProvider = require('./modules/data-provider.js');
@@ -7,20 +8,23 @@ var hostname = 'localhost';
 
 function handleRequest(request, response)
 {
-
-
-	var get_params = url.parse(request.url, true);
-
-	if (get_params.query.image == null)
+	
+		var get_params = url.parse(request.url, true);
+		if (get_params.query.image != null)
 		{
-			dataProvider.provideData('data/data.json',{'Content-Type': 'application/json','Image-Url': 'http://localhost:8102?image' }, response);
+			dataProvider.provideData('images/'+get_params.query.image+'.jpg',{'Content-Type': 'image/jpeg'}, response);
+		}
+		else if (get_params.query)
+		{
+
+				dataProvider.queryData('./data/data.json',{'Content-Type': 'application/json'}, get_params.query, response);
+
 		}
 		else
 		{
-			dataProvider.provideData('images/image.jpg',{'Content-Type': 'image/jpeg'}, response);
+			dataProvider.provideList('data/data.json',{'Content-Type': 'application/json'}, response);
 		}
 
-
-
 }
+
 http.createServer(handleRequest).listen(port, hostname);
