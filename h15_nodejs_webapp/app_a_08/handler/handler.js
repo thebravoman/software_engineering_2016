35c49c26640dd7b7abbd392033742be979a110e8
EvgaDobreva <evg.dobrev@gmail.com>
2016-11-28 20:48:46 +0200

    Homework Updated

--- h15_nodejs_webapp/app_a_08/handler/handler.js
+++ h15_nodejs_webapp/app_a_08/handler/handler.js
@@ -1,8 +1,8 @@
 var fs=require("fs");
 
-exports.provideData=function(file, header_data, response)
+exports.readData=function(file, contentType, response)
 {
-	console.log('providing',file);
+	console.log('providing' + file);
 	fs.exists(file, function(exists)
 	{
 		if (exists)
@@ -16,7 +16,7 @@ exports.provideData=function(file, header_data, response)
 				}
 				else
 				{
-					response.writeHead(200, header_data);
+					response.writeHead(200, contentType);
 					response.end(data);
 				}
 			});
@@ -24,7 +24,71 @@ exports.provideData=function(file, header_data, response)
 		else
 		{
 			response.writeHead(404);
-			response.end('File not found');
+			response.end('Image not found');
 		}
 	});
 }
+
+exports.provideData=function(file, contentType, response)
+{
+	readData(file, contentType, response);
+};
+
+exports.provideList=function(file, contentType, response)
+{
+	readData(file, contentType, response);
+};
+
+exports.queryData=function(file, headers, query, response)
+{
+	fs.exists(file, function(exists)
+	{
+		if (exists)
+		{
+			fs.readFile(file, function(error, data)
+			{
+				if (!error)
+				{
+					var filteredData=[];
+					var j;
+					var all=JSON.parse(data);
+					if (Array.isArray(all.characters))
+					{
+						all.characters.forEach(function(character)
+						{
+							j=0;
+							for (key in query)
+							{
+								if (character[key]===query[ke])
+								{
+									j++;
+								}
+								if (j==Object.keys(query).length)
+								{
+									filtered.push(character);
+								}
+							}
+						});
+					}
+					if (filtered.length>0)
+					{
+						var imageUrl='images/'+query.type;
+						headers["Image-Url"]='http://localhost:8108/?image' + query.type;
+					}
+					response.writeHead(200, headers);
+					response.end(JSON.stringify(filtered));
+				}
+				else
+				{
+					response.writeHead(500);
+					response.end('Internal Server Error');
+				}
+			});
+		}
+		else
+		{
+			response.writeHead(404);
+			response.end('Image not found');
+		}
+	});
+};
