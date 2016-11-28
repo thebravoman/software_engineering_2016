f759f55f823af4c4d01076d363041ee92c36925f
ivanmilevtues <ivanmilevtues@gmail.com>
2016-11-28 11:49:03 -0800

    Tabs Fixed

--- h15_nodejs_webapp/app_a_11/modules/handler.js
+++ h15_nodejs_webapp/app_a_11/modules/handler.js
@@ -26,40 +26,40 @@ exports.provideData = function(filename, headerInfo, response) {
 exports.queryData = function(filename, headers, query, response) {
 	fs.exists(filename, function(exists) {
 		if (exists) {
-				fs.readFile(filename, function(error, data) {
-					if (!error)	{
-						var result = {};
-						var filteredData = [];
-						var allData = JSON.parse(data);
-						if (Array.isArray(allData.characters)){
-                                allData.characters.forEach(function(character) {
-                                    console.log("In character");
-                                    for(var key in query) {
-                                        if(key in character) {
-                                            console.log("Searching eq key..")
-                                            if (character[key] == query[key]) {
-                                                console.log("Pushing Element....")
-                                                filteredData.push(character);
-                                            }
+			fs.readFile(filename, function(error, data) {
+				if (!error)	{
+					var result = {};
+					var filteredData = [];
+					var allData = JSON.parse(data);
+					if (Array.isArray(allData.characters)){
+                            allData.characters.forEach(function(character) {
+                                console.log("In character");
+                                for(var key in query) {
+                                    if(key in character) {
+                                        console.log("Searching eq key..")
+                                        if (character[key] == query[key]) {
+                                            console.log("Pushing Element....")
+                                            filteredData.push(character);
                                         }
                                     }
-                                });
-						}
-						if (filteredData.length > 0) {
-							result = filteredData;
-							// var imageUrl = 'images/' + key;
-							headers["Image-Url"] = 'http://localhost:8111/?image='+filteredData.type;
-						}
+                                }
+                            });
+					}
+					if (filteredData.length > 0) {
+						result = filteredData;
+						// var imageUrl = 'images/' + key;
+						headers["Image-Url"] = 'http://localhost:8111/?image='+filteredData.type;
+					}
 
 
-						response.writeHead(200, headers);
-						response.end(JSON.stringify(result));
-					}
-					else {
-						response.writeHead(500);
-						response.end('Internal Server Error');
-					}
-				});
+					response.writeHead(200, headers);
+					response.end(JSON.stringify(result));
+				}
+				else {
+					response.writeHead(500);
+					response.end('Internal Server Error');
+				}
+			});
 		}
 		else
 		{
