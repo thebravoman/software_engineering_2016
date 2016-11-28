20ddcc335b4ec181263e1b76e5bb002166564a63
nikolay <nikolaystanishev>
2016-11-28 21:03:03 +0200

    Update version to 1.0.1

--- h15_nodejs_webapp/app_a_23/app.js
+++ h15_nodejs_webapp/app_a_23/app.js
@@ -1,7 +1,20 @@
 var http = require('http');
+var url = require('url');
 var port = 8123;
 var handle = require('./modules/handler.js');
 
+function handleRequest(request, response) {
+	var get_params = url.parse(request.url, true);
+	if(get_params.query.image) {
+		console.log('image/' + get_params.query.image + '.jpg');
+		handle.image('./image/' + get_params.query.image + '.jpg', response);
+	} else if(Object.keys(get_params.query).length) {
+		handle.data('./data/data.json', get_params.query, response);
+	} else {
+		handle.allData('./data/data.json', response);
+	}
+}
+
 console.log('listening on localhost:' + port);
 
-http.createServer(handle.handleRequest).listen(port, '127.0.0.1');
+http.createServer(handleRequest).listen(port, '127.0.0.1');
