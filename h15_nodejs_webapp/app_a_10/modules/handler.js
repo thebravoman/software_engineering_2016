f30004a8cfbad906d5481fd8d1a4a11862dd5924
Iva <ivazahova@abv.bg>
2016-11-28 23:15:31 +0200

    aaa

--- h15_nodejs_webapp/app_a_10/modules/handler.js
+++ h15_nodejs_webapp/app_a_10/modules/handler.js
@@ -23,3 +23,52 @@ exports.provideData = function(filename, headerContent, response)
 		}
 	});
 };
+
+exports.queryData = function(filename, headers, key, query, response)
+{
+	fs.exists(filename, function(exists) 
+	{
+		if (exists) 
+		{
+			fs.readFile(filename, function(error, data) 
+			{
+				if (!error) 
+		        	{
+					var result = {};
+					var filteredData = [];
+					var allData = JSON.parse(data);
+					
+					if (Array.isArray(allData.characters)) 
+					{
+                        			allData.characters.forEach(function(character) 
+                        			{
+							if (character[key] === query) 
+							{
+								filteredData.push(character);
+							}
+						});
+					}
+					if (filteredData.length > 0)
+					{
+						result[query] = filteredData;
+						var imageUrl = 'image/' + query;
+						headers["Image-Url"] = 'http://localhost:8110/?image='+query;
+					}
+					response.writeHead(200, headers);
+                    			console.log(result);
+					response.end(JSON.stringify(result));
+					}
+				else 
+				{
+					response.writeHead(500);
+					response.end('Internal Server Error');
+				}
+			});
+		}
+		else
+		{
+			response.writeHead(404);
+			response.end('Image not found');
+		}
+	});
+};
