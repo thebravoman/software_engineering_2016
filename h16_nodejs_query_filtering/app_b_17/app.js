408ae25650cbdd514bc9149aabf881bd5f459148
lmladenov <lachomladenov@gmail.com>
2016-11-28 22:30:36 +0200

    Update app.js

--- h16_nodejs_query_filtering/app_b_17/app.js
+++ h16_nodejs_query_filtering/app_b_17/app.js
@@ -2,7 +2,7 @@ var http = require('http');
 var url = require('url');
 var dataProvider = require('./modules/data-provider.js');
 
-var port = 8180;
+var port = 8217;
 var hostname = 'localhost';
 
 function handleRequest(request, response)
