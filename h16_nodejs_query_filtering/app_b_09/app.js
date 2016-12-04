b9c8329d96b0a46d9a0febe400fe9652e30caa29
GeorgiNzm <lighting_inc@abv.bg>
2016-12-04 23:25:38 +0200

    Updating 16th NodeJS homework - port update

--- h16_nodejs_query_filtering/app_b_09/app.js
+++ h16_nodejs_query_filtering/app_b_09/app.js
@@ -3,7 +3,7 @@ var url = require('url');
 
 var handlerModule = require('./modules/handler.js');
 
-const port = 8888;
+const port = 8209;
 const hostname = 'localhost';
 console.log("listening on localhost: port -> ", port);
 http.createServer(handlerModule.processGETRequest).listen(port, hostname);
