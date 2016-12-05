ab8fae0b5bae4d787fc9a7c23254318bf1adf4b5
ivaylo99 <ivaylo_vasilev99@abv.bg>
2016-12-05 23:22:01 +0200

    Update index.js

--- h13_query_to_json/app_b_14/index.js
+++ h13_query_to_json/app_b_14/index.js
@@ -1,16 +1,14 @@
-var url = require('url');
 var http = require('http');
-var contentType = require('content-type');
+var url = require('url');
+var contentType = require('content-type'); 
 
-
-function Request_Handle(request, response) {
-
-     contentType.format({type : "application/json"});
-     var get_data = url.parse(request.url, true);
-	  	var data_query = get_data.query;
-     response.end(JSON.stringify(data_query, null, 2));
-
+function handleRequest(request, response)
+{
+    response.writeHead(200, {
+        'Content-Type' : 'application/json'        
+    });
+    var get_params = url.parse(request.url, true);
+    var get_query = get_params.query;
+    response.end(JSON.stringify(get_query, null, 2));
 }
-
-
-http.createServer(Request_Handle).listen(8214, '127.0.0.1');
+http.createServer(handleRequest).listen(8214, '127.0.0.1');
