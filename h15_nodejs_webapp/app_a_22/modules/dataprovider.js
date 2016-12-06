56fa9fdb0a9f57bc97ab35d88d36033ffcea4b3f
FarbyPeckers <mihailernandes@gmail.com>
2016-12-06 13:49:24 +0200

    crtl+c ctrl+p

--- h15_nodejs_webapp/app_a_22/modules/dataprovider.js
+++ h15_nodejs_webapp/app_a_22/modules/dataprovider.js
@@ -1,109 +1,83 @@
-var fs = require('fs');
+let fs = require('fs');
 
-function readData(filename, contentType, response) {
-
-    fs.exists(filename, function(exists) {
-
-        if (exists) {
-
-            fs.readFile(filename, function(error, data) {
-
-                if (!error) {
-
-                    response.writeHead(200, contentType);
-                    response.end(data);
-                } else {
-
-                    response.writeHead(500);
-                    response.end('Internal Server Error');
-                }
-            });
-        } else {
-
-            response.writeHead(404);
-            response.end('Image not found');
-
-        }
-    });
+function readData(filename, contentType, response)
+{
+	console.log('providing ' + filename);
+	fs.exists(filename, function(exists) {
+		if (exists) {		
+				fs.readFile(filename, function(error, data) {	
+					if (!error)	{
+						response.writeHead(200, contentType);
+						response.end(data);
+					}
+					else {			
+						response.writeHead(500);
+						response.end('Internal Server Error');
+					}
+				});
+		}
+		else
+		{
+			response.writeHead(404);
+			response.end('Image not found');
+		}
+	});	
 }
 
 
-exports.provideData = function(filename, contentType, response) {
-    readData(filename,contentType, response);
+
+exports.provideData = function(filename, contentType, response)
+{
+	readData(filename,contentType, response);
 };
 
-exports.provideList = function(filename, contentType,  response) {
-    readData(filename,contentType, response);
+exports.provideList = function(filename, contentType,  response)
+{
+	readData(filename,contentType, response);
 };
 
 exports.queryData = function(filename, headers, query, response) {
-
-    fs.exists(filename, function(exists) {
-
-        if (exists) {
-
-            fs.readFile(filename, function(error, data) {
-
-                if (!error) {
-
-                    var filteredData = [];
-                    var allData = JSON.parse(data);
-
-                    if (Array.isArray(allData.characters)) {
-
-                        allData.characters.forEach(function(character) {
-
-                            let equal = false;
-                           
-                            for(let word in query) {
-
-                                if(word in character) {
-                                   
-                                    if(character[word] == query[word]) {
-
-                                        equal = true;
-                                    }
-
-                                }
-                            }
-                            if(equal) {
-
-                                filteredData.push(character);
-
-                            }
-
-                        });
-                    }
-               
-                   /* if (filteredData.length > 0) {
-
-                        if(filteredData[0].type == undefined){
-
-                            headers["Image-Url"] = "Cannot extract image URL from character type";
-
-                        }
-                        else{
-
-                            headers["Image-Url"] = 'http://localhost:8180/?image='+filteredData[0].type;
-
-                        }
-                    }*/
-
-                    response.writeHead(200, headers);
-                    response.end(JSON.stringify(filteredData));
-
-                } else {
-
-                    response.writeHead(500);
-                    response.end('Internal Server Error');
-
-                }
-            });
-        } else {
-
-            response.writeHead(404);
-            response.end('Image not found');
-
-        }
-    });
+	fs.exists(filename, function(exists) {
+		if (exists) {		
+				fs.readFile(filename, function(error, data) {	
+					if (!error)	{
+						let result = {};
+						let filteredData = [];
+						let allData = JSON.parse(data);
+						if (Array.isArray(allData.characters)){
+							allData.characters.forEach(function(character) {
+								let match = true;
+								for(let key in query){
+									if(query[key] != character[key]){
+										match = false;
+										break;
+									}
+								}
+								if(match){
+									filteredData.push(character);
+								}
+							});
+						}
+						if (filteredData.length > 0) {
+							result = filteredData;
+							let imageUrl = 'images/' + query.type;
+							headers["Image-Url"] = 'http://localhost:8122/?image=' + query.type;
+						}
+						
+							
+						response.writeHead(200, headers);
+						response.end(result);
+					}
+					else {			
+						response.writeHead(500);
+						response.end('Internal Server Error');
+					}
+				});
+		}
+		else
+		{
+			response.writeHead(404);
+			response.end('Image not found');
+		}
+	});	
 };
