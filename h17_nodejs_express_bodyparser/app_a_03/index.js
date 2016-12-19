85b68ca1fabea4f836dd0f01e51044150e8b9321
jobo <bojidar.stan@gmail.com>
2016-12-19 18:13:09 +0200

    Update index.js

--- h17_nodejs_express_bodyparser/app_a_03/index.js
+++ h17_nodejs_express_bodyparser/app_a_03/index.js
@@ -11,25 +11,21 @@ var app = express();
 app.use(bodyParser.json());
 
 var router = express.Router();
-router.get('/', function(request, response, next){
-	
-	
-	var get_params = request.query;
-	if (get_params.image)
-	{
-		dataProvider.provideData('images/'+get_params.image+'.jpg', {"Content-Type":"image/pgn"}, response);
-	}
-	else if (Object.keys(get_params).length)
-	{
-		dataProvider.queryData('data/data.json', get_params, response);
-	}
-	else
-	{
-		dataProvider.provideList('data/data.json', "json", response);
-	}
 
-});
+router.get('/', (req, res) => {
+		let get_params = url.parse(req.url, true);
+		  
+		if (get_params.query.image !=null) {
+			dataProvider.provideData('images/'+get_params.query.image+'.jpg','image/jpeg', res);
+		}
+		else if (Object.keys(get_params).length > 0) {
+			dataProvider.queryData('data/data.json', get_params.query, res);
+		}
+		else {
+			dataProvider.provideList('data/data.json', res);
+		}
+	});	
 
 app.use('/', router);
 
-http.createServer(app).listen(8103, 'localhost');
+http.createServer(app).listen(port, 'localhost');
