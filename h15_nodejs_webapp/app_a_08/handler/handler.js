e9d7c24a9b78d0c5247f236a86994b2603b596cc
EvgaDobreva <evg.dobrev@gmail.com>
2016-12-19 15:04:43 +0200

    Fix Homework

--- h15_nodejs_webapp/app_a_08/handler/handler.js
+++ h15_nodejs_webapp/app_a_08/handler/handler.js
@@ -1,31 +1,35 @@
-const fs = require("fs");
+const fs=require("fs");
 
-exports.provideData = function(filename, headerContent, response)
+exports.provideData=function(filename, headerContent, response)
 {
 	console.log("Providing " + filename);
 	fs.exists(filename, function(exists)
 	{
 		if(exists)
 		{
-			fs.readFile(filename, function(error, data){
-              		if (error)
+			fs.readFile(filename, function(error, data)
 			{
-                  		response.writeHead(500);
-                    		response.end("Internal Server Error");
-                	} else {
-                    		response.writeHead(200, headerContent);
-                    		response.end(data);
-			}
-		});
-		} else {
+				if (error)
+				{
+					response.writeHead(500);
+               response.end("Internal Server Error");
+            }
+            else
+            {
+           		response.writeHead(200, headerContent);
+           		response.end(data);
+				}
+			});
+		} 
+		else
+		{
 			response.writeHead(404);
 			response.end("Image not found");
 		}
 	});
 };
 
-
-exports.queryData = function(filename, headers, query, response)
+exports.queryData=function(filename, headers, query, response)
 {
 	fs.exists(filename, function(exists)
 	{
@@ -35,24 +39,24 @@ exports.queryData = function(filename, headers, query, response)
 				{	
 					if (!error)
 					{
-						var result = {};
-						var filteredData = [];
-						var allData = JSON.parse(data);
+						var result={};
+						var filteredData=[];
+						var allData=JSON.parse(data);
 						if (Array.isArray(allData.characters))
 						{
 							allData.characters.forEach(function(character)
 							{
-								if (character.type === query)
+								if (character.type===query)
 								{
 									filteredData.push(character);
 								}
 							});
 						}
-						if (filteredData.length > 0)
+						if (filteredData.length>0)
 						{
-							result[query] = filteredData;
-							var imageUrl = 'images/' + query;
-							headers["Image-Url"] = 'http://localhost:8108/?image='+query;
+							result[query]=filteredData;
+							var imageUrl='image/'+query;
+							headers["Image-Url"]='http://localhost:8108/?image='+query;
 						}						
 						response.writeHead(200, headers);
 						response.end(JSON.stringify(result));
