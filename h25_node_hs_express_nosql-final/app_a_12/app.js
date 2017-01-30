1a8109aa17ab07cc239a19d6a503e00f1bd8ae73
ivanganchev <ivan_ganchev45@abv.bg>
2017-01-30 23:49:12 +0200

    moje li da ne si go prichinqvam nikoga poveche

---
+++ h25_node_hs_express_nosql-final/app_a_12/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const http = require('http');
+
+const routes = require('./routes/index');
+
+const app = express();
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+http.createServer(app).listen(8112, function(){
+	console.log('Express server listening on port 8112');
+});
+
