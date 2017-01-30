6dc72b8450cde59ccabfdacd21282bdb3330b08e
nikolaystanishev <stanishev.nikolay@gmail.com>
2017-01-30 13:50:22 +0200

    Add Homework 25

---
+++ h25_node_hs_express_nosql-final/app_a_23/app.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var http = require('http')
+
+var routes = require('./routes/index')
+
+var app = express()
+
+var port = 8123
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', routes)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found')
+    err.status = 404
+    next(err)
+})
+
+http.createServer(app).listen(port, function(){
+    console.log('Express server listening on port ' + port)  
+})
---
+++ h25_node_hs_express_nosql-final/app_a_23/modules/mongodb-provider.js
@@ -0,0 +1,132 @@
+var models = require('../model/character.js')
+
+var Character = models.Character
+
+var port = 8123
+
+exports.provideList = function(response) {
+    Character.find({}, function(error, result) {
+        if (error) {
+            return null
+        }
+        if (result != null) {
+            response.json(result)
+        }
+    })
+}
+
+exports.queryData = function(queryType, response) {
+    Character.find({type: queryType}, function(error, result) {
+        if (error) {
+            return null
+        }
+        if (result != null) {
+            response.writeHead(200, {
+                'Content-Type': 'aplication/json',
+                'Image-Url': 'http://localhost:' + port + '/' + queryType + '/image'
+            })
+            response.end(JSON.stringify(result))
+        }
+    })
+}
+
+exports.saveCharacter = function(request, response) {
+    var character = toCharacter(request.body)
+    character.save(function(error) {
+        if (!error) {
+            response.writeHead(201, {
+                'Content-Type': 'aplication/json'
+            })
+            response.end(JSON.stringify(request.body))
+        } else {
+            Character.findOne(
+                    {
+                        firstname: character.firstname
+                    },
+                    function(error, result) {
+                        if (error) {
+                            response.writeHead(500, {
+                                'Content-Type': 'text/plain'
+                            })
+                            response.end('Internal Server Error')
+                        } else {
+                            if (!result) {
+                                character.save()
+                                response.writeHead(201, {
+                                    'Content-Type': 'application/json'
+                                })
+                                response.end(JSON.stringify(request.body))
+                            } else {
+                                result.firstname = character.firstname
+                                result.lastname = character.lastname
+                                result.strength = character.strength
+                                result.type = character.type
+                                result.imageUrl = character.imageUrl
+                                result.save()
+                                response.json(request.body)
+                            }
+                        }
+                    }
+                )
+        }
+    })
+}
+
+exports.saveImage = function(request, response) {
+    var writeStream = models.Grid.createWriteStream({
+        _id: request.params.type,
+        filename: 'image',
+        mode: 'w'
+    })
+
+    writeStream.on('error', function(error) {
+        response.send('500', 'Internal Server Error')
+        return
+    })
+
+    writeStream.on('close', function() {
+        var readStream = models.Grid.createReadStream({
+            _id: request.params.type,
+            filename: 'image',
+            mode: 'r'
+        })
+
+        response.on('error', function(error) {
+            response.send('500', 'Internal Server Error')
+            return
+        })
+
+        response.writeHead(200, {
+            'Content-Type': 'image/jpeg'
+        })
+        readStream.pipe(response)
+    })
+
+    request.pipe(writeStream)
+}
+
+exports.getImage = function(request, response) {
+    var readStream = models.Grid.createReadStream({
+        _id: request.params.type,
+        filename: 'image',
+        mode: 'r'
+    })
+    
+    readStream.on('error', function(error) {
+        response.send('500', 'Internal Server Error')
+        return
+    })
+    
+    response.writeHead(200, {'Content-Type' : 'image/jpeg'})
+    readStream.pipe(response)
+}
+
+function toCharacter(characterObject) {
+    return new Character({
+        firstname: characterObject.firstname,
+        lastname: characterObject.lastname,
+        strength: characterObject.strength,
+        imageUrl: characterObject.imageUrl,
+        type: characterObject.type
+    })
+}
