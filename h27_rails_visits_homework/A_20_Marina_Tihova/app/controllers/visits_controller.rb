b2b1cd1ea41960dd2dd86bd4e37c23fc33eaf5ed
MarchiT <mrtihova@gmail.com>
2017-01-31 18:37:20 +0200

    ...

---
+++ h27_rails_visits_homework/A_20_Marina_Tihova/app/controllers/visits_controller.rb
@@ -0,0 +1,27 @@
+class VisitsController < ApplicationController
+  def get_id
+    user_id = params[:user]
+
+    if Visit.exists?(user_id)
+      @curr_visitor = Visit.find(user_id)
+      render :json => {"count" => @curr_visitor.count}, content_type: "application/json"
+    else
+      render :json => {"count" => 0}, content_type: "application/json"
+    end
+  end
+
+  def post_id
+    user_id = params[:user]
+
+    if !Visit.exists?(user_id)
+      @curr_visitor = Visit.new(:user_id => user_id, :count => 0)
+      @user_visitor.id = user_id
+    else
+      @curr_visitor = Visit.find(user_id)
+    end
+
+    @curr_visitor.increment_counter
+    @curr_visitor.save
+
+  end
+end
