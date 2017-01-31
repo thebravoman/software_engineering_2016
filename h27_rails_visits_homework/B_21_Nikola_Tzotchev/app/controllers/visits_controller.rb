80492090dcdb01104174597b6de50855df2c7e8d
nikolatzotchev <ntzotchev@gmail.com>
2017-01-31 14:33:48 +0200

    fixerion programerino

--- h27_rails_visits_homework/B_21_Nikola_Tzotchev/app/controllers/visits_controller.rb
+++ h27_rails_visits_homework/B_21_Nikola_Tzotchev/app/controllers/visits_controller.rb
@@ -4,7 +4,15 @@ class VisitsController < ApplicationController
   end
 
   def show
-    @vis = Visit.find(params[:user])
+    if !Visit.exists?(params[:user])
+      @vis = create
+    end
+      @vis = Visit.find(params[:user])
+        @json_res = {"count": @vis.visited}.to_json
+        respond_to do |format|
+          format.json {render :json => @json_res}
+        end
+      #tuka trqbwa da kaja izri4no da e json i show.html trqbwa da go mahna
   end
   def create
     if Visit.exists?(params[:user])
