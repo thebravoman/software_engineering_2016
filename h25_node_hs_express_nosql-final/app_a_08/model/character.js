41f46124bafa80d84223a8bb5a282a2432d72a78
Evga_Dobreva <evg.dobrev@gmail.com>
2017-01-31 20:46:40 +0200

    Homework

--- h25_node_hs_express_nosql-final/app_a_08/model/character.js
+++ h25_node_hs_express_nosql-final/app_a_08/model/character.js
@@ -1,19 +1,20 @@
-const var mongoose=require('mongoose');
-const var Schema=mongoose.Schema;
-const var GridImage=require('gridfs-stream');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var GridImage = require('gridfs-stream');
 
 mongoose.connect('mongodb://localhost/characters');
 
-var characterSchema=new Schema 
-({
-    firstname: {type: String, index: {unique: true}},
-    lastname: String,
-    strength: Number,
-    imageUrl: String,
-    type: String
+
+var characterSchema = new Schema ({	
+	firstname : {type: String, index: {unique: true}},
+	lastname : String,
+	strength : Number,
+	imageUrl : String,
+	type : String	
 });
 
-var Character=mongoose.model('Character', characterSchema);
-var GridImage=GridImage(mongoose.connection.db, mongoose.mongo);
+var Character = mongoose.model('Character', characterSchema);
+var GridImage = GridImage(mongoose.connection.db, mongoose.mongo);
 
-module.exports={ Character: Character, Grid: GridImage };
+module.exports = {Character : Character,
+Grid : GridImage};
--- h25_node_hs_express_nosql-final/app_a_08/routes/index.js
+++ h25_node_hs_express_nosql-final/app_a_08/routes/index.js
@@ -1,29 +1,32 @@
-const var express = require('express');
-const var router = express.Router();
-const var dbProvider = require('../modules/mongodb-provider.js');
+var express = require('express');
+var router = express.Router();
+var dataProvider = require('../modules/data-provider.js');
 
-router.get('/', function(request, response, next) 
-{
-    dbProvider.provideList(response);
+var dbProvider = require('../modules/mongodb-provider.js');
+
+/* GET home page. */
+
+
+router.get('/', function(request, response, next){
+	console.log('Get all');
+	dbProvider.provideList( response);
+})
+
+router.post('/', function(request, response, next){
+	dbProvider.saveCharacter(request, response);
 });
 
-router.post('/', function(request, response, next)
-{
-    dbProvider.saveCharacter(request, response);
+router.post('/:type/image', function(request, response, next){
+	dbProvider.saveImage(request, response);
 });
 
-router.post('/:type/image', function(request, response, next)
-{
-    dbProvider.saveImage(request, response);
+router.get('/:type/image', function(request, response, next){
+	dbProvider.getImage(request, response);
+
 });
 
-router.get('/:type/image', function(request, response, next)
-{
-    dbProvider.getImage(request, response);
-});
-
-router.get('/:type', function(request, response, next) {
-    dbProvider.queryData(request.params.type, response);
+router.get('/:type', function(request, response, next){
+	dbProvider.queryData({}, request, response);
 });
 
 module.exports = router;
